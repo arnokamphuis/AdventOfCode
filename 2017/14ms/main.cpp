393bc355f5f5fb3a5b0ca614841b5fe42c0c94ec
Arno Kamphuis <ik@arnokamphuis.nl>
2017-12-14 09:04:23 +0100

    Solution to part 1 of 14ms of 2017

---
+++ 2017/14ms/main.cpp
@@ -0,0 +1,98 @@
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <bitset>
+#include <algorithm>
+
+std::string knot_hash(std::string line) {
+    std::vector<int> lengths, orig_lengths;
+    for (auto c: line)
+        lengths.push_back((int)c);
+
+    lengths.push_back(17);
+    lengths.push_back(31);
+    lengths.push_back(73);
+    lengths.push_back(47);
+    lengths.push_back(23);
+
+    orig_lengths = lengths;
+
+    int maximum_round = 64;
+    int round_counter = 0;
+
+    int skip_size=0;
+    int listlength = 256;
+
+    std::vector<int> list;
+    for (int i=0; i<listlength; ++i) list.push_back(i);
+
+    int position = 0;
+
+    for (round_counter = 0; round_counter < maximum_round; ++round_counter) {
+        lengths = orig_lengths;
+        while (lengths.size()>0) {
+            // get the length
+            int length = *lengths.begin(); lengths.erase( lengths.begin() ); 
+
+            std::vector<int> rev;
+            for (int i=0; i<length; ++i)
+                rev.insert( rev.begin(), list[ (position+i) % listlength ] );
+            
+            for (int i=0; i<length; ++i)
+                list[ (position+i) % listlength ] = rev[i];
+            
+            position += length + skip_size;
+            position = position % listlength;
+
+            // increase skip size
+            ++skip_size;
+        }
+    }
+
+    std::vector<int> densehash;
+    for (int i=0; i<16; i++) {
+        int xorvalue = list[16*i];
+        for (int j = 1; j<16; j++)
+            xorvalue = xorvalue ^ list[16*i+j];
+        densehash.push_back(xorvalue);
+    }
+
+    std::stringstream ss;
+    for (auto d : densehash) ss << std::setfill('0') << std::setw(2) << std::hex << d;
+    return ss.str();    
+}
+
+std::string hash_to_bits(std::string hash) {
+    std::string res = "";
+
+    for (auto c: hash) {
+        std::stringstream ss;
+        ss << std::hex << c;
+        unsigned int uc;
+        ss >> uc;
+        std::bitset<32> bits(uc);
+
+        res += std::to_string((int)bits[3]);
+        res += std::to_string((int)bits[2]);
+        res += std::to_string((int)bits[1]);
+        res += std::to_string((int)bits[0]);
+    }
+    return res;
+}
+
+int main() {
+
+    std::string input;
+    std::cin >> input;
+
+    int counter = 0;
+    for (int row=0;row<128;++row) {
+        std::string key = input + "-" +  std::to_string(row);
+        std::string hash = knot_hash(key);
+        std::string bits = hash_to_bits(hash);
+        counter += std::count(bits.begin(), bits.end(), '1');
+    }
+    std::cout << "Part 1: " << counter << std::endl;
+}
