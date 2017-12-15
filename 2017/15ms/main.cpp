7d4e773d6f4f82a676e426222f5683e4ef58fde4
arnokamphuis <ik@arnokamphuis.nl>
2017-12-15 09:50:05 +0100

    Solution to 15ms of 2017

--- 2017/15ms/main.cpp
+++ 2017/15ms/main.cpp
@@ -2,40 +2,53 @@
 #include <bitset>
 #include <cstdint>
 
-bool compare(uint_fast32_t a, uint_fast32_t b) {
+bool compare(uint64_t a, uint64_t b) {
     bool res = true;
-    std::bitset<16> bs_a(a);
-    std::bitset<16> bs_b(b);
-    for (int j=0;j<16;++j)
-        if (bs_a[j] != bs_b[j])
-            res = false;
-    return res;
+     std::bitset<16> bs_a(a);
+     std::bitset<16> bs_b(b);
+     for (int j=0;j<16;++j)
+         if (bs_a[j] != bs_b[j])
+             res = false;
+     return res;
 }
 
-uint_fast32_t generate(uint_fast32_t prev, uint_fast32_t seed) {
-    return ((prev * seed) % 2147483647);
+uint64_t generate(uint64_t prev, uint64_t seed, uint64_t multiple) {
+    uint64_t temp = prev * seed;
+    uint64_t candidate;
+    
+    do {
+        candidate = ((prev * seed) % (uint64_t)2147483647);
+        prev = candidate;
+    } while ( (candidate % multiple) != 0);
+
+    return candidate;
 }
 
 int main() {
-    uint_fast32_t val_a, val_b;
+    uint64_t val_a, val_b;
     
-    uint_fast32_t init_a = 65;
-    uint_fast32_t init_b = 8921;
+    // real puzzle values
+    uint64_t init_a = 516;
+    uint64_t init_b = 190;
 
-    uint_fast32_t found=0;
-    uint_fast32_t counter=0;
+    // test puzzle values
+    // uint64_t init_a = 65;
+    // uint64_t init_b = 8921;
+
+    uint64_t found=0;
+    uint64_t counter=0;
 
     val_a = init_a;
     val_b = init_b;
 
-    while (counter<5) {
-        val_a = generate(val_a, 16807);
-        val_b = generate(val_b, 48271);
-        std::cout << val_a << "\t" << val_b << std::endl;
+    while (counter<5000000) {
+        val_a = generate(val_a, 16807, 4);
+        val_b = generate(val_b, 48271, 8);
+        //std::cout << val_a << "\t\t" << val_b << std::endl;
         if (compare(val_a,val_b))
             ++found;
         ++counter;
     }
 
-    std::cout << "Part 1: " << found << std::endl;
+    std::cout << "Answer: " << found << std::endl;
 }
