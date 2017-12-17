0efb5f185535ff4c1a3899f510606f1d901dcfd1
Arno Kamphuis <ik@arnokamphuis.nl>
2017-12-17 09:05:39 +0100

    Added comments to the solution of 17ms for 2017

--- 2017/17ms/main.cpp
+++ 2017/17ms/main.cpp
@@ -13,37 +13,37 @@ int main() {
     int input = 366; // real 366, test 3
 
     std::vector<int> circular_buffer;
-
     circular_buffer.push_back(0);
     auto current = circular_buffer.begin();
 
     for (int i=0;i<2017;++i) {
-        //std::cout << "going to insert: " << (i+1) << " with step " << input << std::endl;
+        // find the number of steps to take forward
         int steps_forward = input % circular_buffer.size();
-        //std::cout << "step forward: " << steps_forward << std::endl;
+        // take steps forward
         for (int j=0;j<steps_forward;++j) {
             ++current;
+            // if move beyond end, start at beginning
             if (current==circular_buffer.end()) current = circular_buffer.begin();
         }
-        //std::cout << "current value at that position: " << *current << std::endl;
+        // insert value and update the current iterator to that position
         current = circular_buffer.insert(current+1, i+1);
-
-        //std::cout << "-------- buffer = " ;
-        //for (auto cb: circular_buffer) std::cout << cb << " "; std::cout << std::endl;
     }
 
     std::cout << "Part 1: " << *(current+1) << std::endl;
 
-    int bufsize = 1;
-    int cp = 0;
-    int iv = -1;
-    int np = 0;
+
+    // Part two is too complicated to fulled expand. So only track the 0 position and the value after it
+    int bufsize = 1; // size of the circular buffer
+    int cp = 0;      // current position for insert
+    int iv = -1;     // last inserted value after 0
+    int np = 0;      // current position of 0
+
     for (int i=0;i<50000000;++i) {
-        int ip = 1+(cp+input)%bufsize;
-        if (ip==(1+np)) iv = i+1;
-        if (ip<=(0+np)) ++np;
-        cp = ip;
-        ++bufsize;
+        int ip = 1+(cp+input)%bufsize;  // find position to insert at
+        if (ip==(1+np)) iv = i+1;       // if insert position if directly after 0, remember value
+        if (ip<=(0+np)) ++np;           // if insert position was before or at position of 0, move 0 position on
+        cp = ip;                        // current position is at the insert position
+        ++bufsize;                      // the buffer has increased by 1
     }
     std::cout << "Part 2: " << iv << std::endl;
 }
