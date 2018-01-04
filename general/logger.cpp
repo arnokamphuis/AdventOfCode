a4a82eb60ab79d558b6912c2606765eb175f541c
Arno Kamphuis <ik@arnokamphuis.nl>
2018-01-04 16:54:59 +0100

    Oops, forgot to add the new logger to the commit

--- general/logger.cpp
+++ general/logger.cpp
@@ -1,1 +1,8 @@
 #include "logger.h"
+
+#include <iostream>
+
+std::ostream& logger::get(logtype lt) {
+    if (lt == logtype::logERROR) return std::cerr;
+    else return std::cout;
+}
--- general/logger.h
+++ general/logger.h
@@ -2,25 +2,13 @@
 #define __LOGGER_H__
 
 #include <ostream>
-#include <iostream>
+
+enum class logtype { logINFO, logERROR };
 
 class logger {
 private:
-    std::ostream& __stream;
-    std::ostream& __err_stream;
 public:
-    logger(std::ostream& s, std::ostream& es) : __stream(s), __err_stream(es) {}
-
-    template <typename T>
-    void log(T v) const {
-        __stream << v;
-    }
-
-    template <typename T>
-    void error(T v) const {
-        __err_stream << v;
-    }
-
+    static std::ostream& get(logtype lt);
 };
 
 #endif // __LOGGER_H__
