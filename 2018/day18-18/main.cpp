df2b0b15666112fece1a2a6fb23b636a973fe9df
Arno Kamphuis <ik@arnokamphuis.nl>
2018-12-18 09:42:35 +0100

    Finished day 18 of 2018

--- 2018/day18-18/main.cpp
+++ 2018/day18-18/main.cpp
@@ -18,9 +18,30 @@ class map {
   int h;
   int size;
   std::vector<groundtype> field;
+  int runs;
+
+  std::map<std::vector<groundtype>, int> history;
 
 public:
-  map() { h = 0; }
+  map() {
+    runs = 0;
+    h = 0;
+  }
+
+  void print() {
+    for (int y = 0; y < w; ++y) {
+      for (int x = 0; x < w; ++x) {
+        int index = x + y * w;
+        if (field[index] == OPEN)
+          std::cout << ".";
+        if (field[index] == TREES)
+          std::cout << "|";
+        if (field[index] == LUMBER)
+          std::cout << "#";
+      }
+      std::cout << std::endl;
+    }
+  }
 
   void addline(std::string line) {
     if (field.size() == 0) {
@@ -47,8 +68,9 @@ public:
     ++h;
   }
 
-  void update() {
-    std::vector<groundtype> newfield;
+  int update() {
+    ++runs;
+    std::vector<groundtype> newfield = field;
     std::map<groundtype, int> counter;
 
     for (int x = 0; x < w; ++x) {
@@ -57,8 +79,15 @@ public:
         counter[OPEN] = 0;
         counter[TREES] = 0;
         counter[LUMBER] = 0;
-        for (int s = -1; s < 2; ++s) {
-          for (int t = -1; t < 2; ++t) {
+
+        int left = ((x == 0) ? 0 : -1);
+        int right = ((x == (w - 1)) ? 0 : 1);
+
+        int top = ((y == 0) ? 0 : -1);
+        int bottom = ((y == (w - 1)) ? 0 : 1);
+
+        for (int s = left; s <= right; ++s) {
+          for (int t = top; t <= bottom; ++t) {
             if (!((s == 0) && (t == 0))) {
               int offindex = (x + s) + (y + t) * w;
               if ((offindex >= 0) && (offindex < size)) {
@@ -69,6 +98,11 @@ public:
         }
 
         groundtype gt = field[index];
+
+        // std::cout << "trans: " << index << "(" << gt << ") : " <<
+        // counter[OPEN]
+        //           << " " << counter[TREES] << " " << counter[LUMBER]
+        //           << std::endl;
 
         if (gt == OPEN) {
           if (counter[TREES] >= 3)
@@ -86,6 +120,36 @@ public:
     }
 
     field = newfield;
+
+    if (history.find(field) != history.end())
+      return runs - history[field];
+    else
+      history[field] = runs;
+    return -1;
+  }
+
+  void doruns(int count) {
+    int delta;
+    for (int i = 0; i < count; ++i) {
+      if ((delta = update()) != -1) {
+        i += delta * ((count - i) / delta);
+      }
+    }
+  }
+
+  int calcscore() {
+    std::map<groundtype, int> counter;
+    counter[OPEN] = 0;
+    counter[TREES] = 0;
+    counter[LUMBER] = 0;
+    for (int x = 0; x < w; ++x) {
+      for (int y = 0; y < w; ++y) {
+        int index = x + y * w;
+        counter[field[index]] += 1;
+      }
+    }
+
+    return counter[TREES] * counter[LUMBER];
   }
 };
 
@@ -99,7 +163,13 @@ int main() {
     input.push_back(line);
   }
 
-  m.update();
+  for (int i = 0; i < 10; ++i)
+    m.update();
+  logger::get(logtype::logINFO) << "Part 1: " << m.calcscore() << "\n";
+
+  int runs = 1000000000;
+  m.doruns(runs - 10);
+  logger::get(logtype::logINFO) << "Part 2: " << m.calcscore() << "\n";
 
   return 0;
 }
