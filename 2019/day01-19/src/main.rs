228f1498159ef52b33f38d064929eb8877042893
Arno Kamphuis <arno.kamphuis@hu.nl>
2019-11-27 19:33:15 +0100

    Added boilerplate code with timing in day 1 and day 2 of 2019

--- 2019/day01-19/src/main.rs
+++ 2019/day01-19/src/main.rs
@@ -1,3 +1,15 @@
+use std::time::{Instant};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Day 1 of 2019");
+
+    let start1 = Instant::now();
+
+    let after1 = Instant::now();
+    println!("Part 1: {}, in {:?}", 0, after1.duration_since(start1));
+
+    let start2 = Instant::now();
+
+    let after2 = Instant::now();
+    println!("Part 2: {}, in {:?}", 0, after2.duration_since(start2));
 }
