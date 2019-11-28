4a029a883182a59c9c756d70fa04b9e8911e9d58
Arno Kamphuis <arno.kamphuis@hu.nl>
2019-11-28 20:37:17 +0100

    FInished day 3 of 2016

---
+++ 2016/src/day03_16.rs
@@ -0,0 +1,24 @@
+// use std::collections::HashSet;
+use std::time::{Instant};
+// use std::char;
+
+use super::tools;
+
+#[allow(dead_code)]
+pub fn run() {
+    println!("Day 3 of 2016");
+
+    let input_file = "./input/day03_16_test.txt";
+    // let input_file = "./input/day03_16_real.txt";
+
+    let start1 = Instant::now();
+
+    let after1 = Instant::now();
+    println!("Part 1: {}, in {:?}", keycode, after1.duration_since(start1));
+
+    let start2 = Instant::now();
+
+
+    let after2 = Instant::now();
+    println!("Part 2: {}, in {:?}", keycode, after2.duration_since(start2));
+}
--- 2016/src/main.rs
+++ 2016/src/main.rs
@@ -1,8 +1,10 @@
 mod tools;
 mod day01_16;
 mod day02_16;
+mod day03_16;
 
 fn main() {
     // day01_16::run();
-    day02_16::run();
+    // day02_16::run();
+    day03_16::run();
 }
