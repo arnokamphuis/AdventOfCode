f1f524add4eccba6af0cf485503aa86438be2d52
Arno Kamphuis <arno.kamphuis@hu.nl>
2019-11-28 12:42:30 +0100

    Added boilerplate 2016

---
+++ 2016/src/day01_16.rs
@@ -0,0 +1,15 @@
+use std::time::{Instant};
+
+pub fn run() {
+    println!("Day 1 of 2016");
+
+    let start1 = Instant::now();
+
+    let after1 = Instant::now();
+    println!("Part 1: {}, in {:?}", 0, after1.duration_since(start1));
+
+    let start2 = Instant::now();
+
+    let after2 = Instant::now();
+    println!("Part 2: {}, in {:?}", 0, after2.duration_since(start2));
+}
---
+++ 2016/src/main.rs
@@ -0,0 +1,6 @@
+mod tools;
+mod day01_16;
+
+fn main() {
+    day01_16::run();
+}
