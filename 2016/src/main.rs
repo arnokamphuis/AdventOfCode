cb29bc882c42934ba8b94c68886f4da54a6d1f79
Arno Kamphuis <arno.kamphuis@hu.nl>
2019-11-28 21:49:32 +0100

    Finished day 4 of 2016

--- 2016/src/main.rs
+++ 2016/src/main.rs
@@ -2,9 +2,11 @@ mod tools;
 mod day01_16;
 mod day02_16;
 mod day03_16;
+mod day04_16;
 
 fn main() {
     // day01_16::run();
     // day02_16::run();
-    day03_16::run();
+    // day03_16::run();
+    day04_16::run();
 }
