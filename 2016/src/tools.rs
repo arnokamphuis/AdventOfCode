01021de388609ec0dba8e14d54c16fb9260f6e98
Arno Kamphuis <ik@arnokamphuis.nl>
2019-11-30 20:38:33 +0100

    Added the tools

--- 2016/src/tools.rs
+++ 2016/src/tools.rs
@@ -9,3 +9,15 @@ where
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
+
+pub fn get_input(filename: String) -> Vec<String> {
+    let mut input = Vec::new();
+    if let Ok(lines) = read_lines(filename) {
+        for line in lines {
+            if let Ok(value) = line {
+                input.push(value);
+            }
+        }
+    }
+    input
+}
