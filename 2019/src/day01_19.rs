c30b6d205e591845ffef6706d6b09f4c777eb1e5
Arno Kamphuis <ik@arnokamphuis.nl>
2019-12-01 08:22:54 +0100

    Some optimization to the code of day 1 of 2019

--- 2019/src/day01_19.rs
+++ 2019/src/day01_19.rs
@@ -1,10 +1,9 @@
 use std::time::{Instant};
 use super::tools;
-use math::round;
 
 fn calculate_fuel(mass: i64) -> i64 {
-    let fuel_for_mass = round::floor(mass as f64 / 3.0, 0) as i64 - 2;
-    if fuel_for_mass <= 0 { 0 as i64 } else { fuel_for_mass + calculate_fuel(fuel_for_mass) }
+    let fuel_for_mass = mass / 3 - 2;
+    if fuel_for_mass <= 0 { 0 } else { fuel_for_mass + calculate_fuel(fuel_for_mass) }
 }
 
 pub fn run() {
@@ -18,11 +17,11 @@ pub fn run() {
 
     let mut fuel: i64 = input.clone().into_iter()
         .map(|n| n.parse::<i64>().unwrap())
-        .map(|mass| round::floor(mass as f64 / 3.0, 0) as i64 - 2)
+        .map(|mass| mass / 3 - 2)
         .sum::<i64>();
 
     let after1 = Instant::now();
-    println!("Part 1: {}, in {:?}", fuel, after1.duration_since(start1));
+    println!("Part 1: {} (in {:?})", fuel, after1.duration_since(start1));
 
     let start2 = Instant::now();
 
@@ -32,5 +31,5 @@ pub fn run() {
         .sum::<i64>();
 
     let after2 = Instant::now();
-    println!("Part 2: {}, in {:?}", fuel, after2.duration_since(start2));
+    println!("Part 2: {} (in {:?})", fuel, after2.duration_since(start2));
 }
