a462815752e3b18aa12d81bc7d1bd0a88aea1d93
Arno Kamphuis <ik@arnokamphuis.nl>
2019-12-01 08:17:39 +0100

    Finished day 1 of 2019 in a more Rust-like style

--- 2019/src/day01_19.rs
+++ 2019/src/day01_19.rs
@@ -2,19 +2,9 @@ use std::time::{Instant};
 use super::tools;
 use math::round;
 
-fn calculate_fuel(mass: i64, extra: bool) -> i64 {
+fn calculate_fuel(mass: i64) -> i64 {
     let fuel_for_mass = round::floor(mass as f64 / 3.0, 0) as i64 - 2;
-    if fuel_for_mass <= 0 { 
-        0 as i64 
-    } else {
-        let mut fuel = fuel_for_mass;
-        if extra {
-            if fuel > 0 {
-                fuel += calculate_fuel(fuel, extra);
-            }
-        }
-        fuel
-    }
+    if fuel_for_mass <= 0 { 0 as i64 } else { fuel_for_mass + calculate_fuel(fuel_for_mass) }
 }
 
 pub fn run() {
@@ -22,29 +12,24 @@ pub fn run() {
 
     // let input_file = "./input/day01_19_test.txt";
     let input_file = "./input/day01_19_real.txt";
+    let input = tools::get_input(String::from(input_file));
 
     let start1 = Instant::now();
 
-    let input = tools::get_input(String::from(input_file));
-
-    let mut fuel: i64 = 0;
-    for line in &input {
-        let mass = line.parse::<i64>().unwrap();
-        let fuel_for_mass = calculate_fuel(mass,false);
-        fuel += fuel_for_mass;
-    }
+    let mut fuel: i64 = input.clone().into_iter()
+        .map(|n| n.parse::<i64>().unwrap())
+        .map(|mass| round::floor(mass as f64 / 3.0, 0) as i64 - 2)
+        .sum::<i64>();
 
     let after1 = Instant::now();
     println!("Part 1: {}, in {:?}", fuel, after1.duration_since(start1));
 
     let start2 = Instant::now();
 
-    fuel = 0;
-    for line in &input {
-        let mass = line.parse::<i64>().unwrap();
-        let fuel_for_mass = calculate_fuel(mass,true);
-        fuel += fuel_for_mass;
-    }
+    fuel = input.clone().into_iter()
+        .map(|n| n.parse::<i64>().unwrap())
+        .map(|mass| calculate_fuel(mass))
+        .sum::<i64>();
 
     let after2 = Instant::now();
     println!("Part 2: {}, in {:?}", fuel, after2.duration_since(start2));
