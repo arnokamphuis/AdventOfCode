ed6dba0e87efb6e5c52d3aa1c89390fcdfec3cba
Arno Kamphuis <arno.kamphuis@hu.nl>
2019-12-04 09:45:49 +0100

    Made day 4 more Rusty

--- 2019/src/day04_19.rs
+++ 2019/src/day04_19.rs
@@ -1,17 +1,11 @@
 use std::time::{Instant};
-// use super::tools;
 
 fn check_rules(pw: &[u8; 6], strict: bool) -> bool {
     let mut increasing = true;
 
     pw.iter().enumerate().for_each( |(index, v)| if index<5 && v > &pw[index+1] { increasing=false; } );
-    // for i in 0..pw.len()-1 {
-    //     if pw[i] > pw[i+1] {
-    //         increasing = false;
-    //     }
-    // }
-    if increasing {
 
+    if increasing {
         let mut counter = [0;10];
         pw.iter().for_each(|n| { counter[*n as usize] += 1; } );
 
@@ -32,6 +26,8 @@ fn check_rules(pw: &[u8; 6], strict: bool) -> bool {
 pub fn run() {
     println!("Day 14 of 2019");
 
+    const RADIX: u32 = 10;
+
     let start0 = Instant::now();    
 
     let after0 = Instant::now();
@@ -42,13 +38,12 @@ pub fn run() {
     let mut code : [u8; 6] = [0; 6];
 
     let mut count = 0;
-    for i in 172851..675870 {
-        const RADIX: u32 = 10;
+    (172851..675870).into_iter().for_each(|i| {
         format!("{}",i).chars().enumerate().for_each(|(i,v)| code[i] = v.to_digit(RADIX).unwrap() as u8);
         if check_rules(&code, false) {
             count += 1;
         }
-    }
+    });
 
     let after1 = Instant::now();
     println!("Part 1: {}, in {:?}", count, after1.duration_since(start1));
@@ -60,6 +55,7 @@ pub fn run() {
         const RADIX: u32 = 10;
         format!("{}",i).chars().enumerate().for_each(|(i,v)| code[i] = v.to_digit(RADIX).unwrap() as u8);
         if check_rules(&code, true) {
+            // println!("{:?}", code);
             count += 1;
         }
     }
