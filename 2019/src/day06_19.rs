fa305c967e4cc4d8ccf0bd6d5b588a88418f5931
Arno Kamphuis <arno.kamphuis@hu.nl>
2019-12-06 08:43:51 +0100

    Finished day 6 of 2019

--- 2019/src/day06_19.rs
+++ 2019/src/day06_19.rs
@@ -1,17 +1,16 @@
 use std::time::{Instant};
 use super::tools;
 use std::collections::HashMap;
+use std::collections::HashSet;
 
 struct Node {
-  name: String,
   parent: String,
   children: Vec<String>,
-  depth: u64,
 }
 
 impl Node {
-    fn new(n: &String) -> Node {
-        Node { name : n.clone(), parent: String::from(""), children: vec![], depth: 0 }
+    fn new() -> Node {
+        Node { parent: String::from(""), children: vec![] }
     }
 
     fn add_child(&mut self, n: &String) {
@@ -22,6 +21,10 @@ impl Node {
         self.parent = p.clone();
     }
 
+    fn get_parent(&self) -> String {
+        self.parent.clone()
+    }
+
     fn get_chidren(&self) -> Vec<String> {
         let mut res: Vec<String> = vec![];
         for child in &self.children {
@@ -29,14 +32,6 @@ impl Node {
         }
         res.clone()
     }
-
-    // fn calculate_orbits(&self, d: u64) -> u64 {
-    //     self.depth = d;
-    //     for c in self.children {
-    //         d += c.calculate_orbits(d+1);
-    //     }
-    //     d
-    // }
 }
 
 struct Tree {
@@ -50,20 +45,18 @@ impl Tree {
 
     fn add_orbit(&mut self, a: &String, b: &String) {
         if !self.nodes.contains_key(b) {
-            self.nodes.insert(b.to_string(),Node::new(b));            
+            self.nodes.insert(b.to_string(),Node::new());            
         }
         if !self.nodes.contains_key(a) {
-            self.nodes.insert(a.to_string(),Node::new(a));
+            self.nodes.insert(a.to_string(),Node::new());
         }
         if let Some(ma) = self.nodes.get_mut(a) {
             (*ma).add_child(b);
-            // println!("adding {} to parent {}", b, a);
         }
         if let Some(mb) = self.nodes.get_mut(b) {
             (*mb).set_parent(a);
-            // println!("adding {} to parent {}", b, a);
-        }
-}
+        }    
+    }
 
     fn calculate_orbits(&mut self, n: String, d: u64) -> u64 {
         let mut total = d;
@@ -71,6 +64,18 @@ impl Tree {
             total += self.calculate_orbits(c, d+1);
         }
         total
+    }
+
+    fn get_parent_set(&self, n: String) -> HashSet<String> {
+        let mut set: HashSet<String> = HashSet::new();
+        set.insert(n.clone());
+        let parent = self.nodes[&n].get_parent();
+        if parent != "" {
+            let parent_set = self.get_parent_set(parent.clone());
+            parent_set.iter().for_each(|p| {
+                set.insert(p.to_string());});
+        }
+        set.clone()
     }
 }
 
@@ -99,6 +104,19 @@ pub fn run() {
 
     let start2 = Instant::now();
 
+    let set_san = orbits.get_parent_set("SAN".to_string());
+    let set_you = orbits.get_parent_set("YOU".to_string());
+
+    let mut path: Vec<String> = vec![];
+    let diff1 = set_san.difference(&set_you);
+    for d in diff1 {
+        path.push(d.clone());
+    }
+    let diff2 = set_you.difference(&set_san);
+    for d in diff2 {
+        path.push(d.clone());
+    }
+
     let after2 = Instant::now();
-    println!("Part 2: {}, in {:?}", 0, after2.duration_since(start2));
+    println!("Part 2: {}, in {:?}", path.len()-2, after2.duration_since(start2));
 }
