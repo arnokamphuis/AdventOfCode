37186e008e673e6e5f961f7314c228348a2cd8ca
Arno Kamphuis <ik@arnokamphuis.nl>
2019-12-09 16:09:21 +0100

    Finished day 9 of 2019

--- 2019/src/day09_19.rs
+++ 2019/src/day09_19.rs
@@ -1,10 +1,11 @@
 use super::tools;
 use std::time::Instant;
-use std::collections::HashMap;
+// use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 #[derive(Clone, Hash)]
 struct IntCodeComputer {
-    memory: HashMap<usize,i64>,
+    memory: BTreeMap<i64, i64>,
     pc: i64,
     relative_base: i64,
     input: Vec<i64>,
@@ -15,49 +16,55 @@ struct IntCodeComputer {
 enum Mode {
     POS = 1,
     IMM,
+    REL,
 }
 
 impl IntCodeComputer {
-    fn new(m: &Vec<i64>) -> IntCodeComputer {
+    fn new(m: &BTreeMap<i64, i64>) -> IntCodeComputer {
         IntCodeComputer {
             memory: m.clone(),
             pc: 0,
+            relative_base: 0,
             input: vec![],
             output: vec![],
         }
     }
 
-    fn get_mem_index(&self, index: i64, mode: usize) -> i64 {
-        let mut real_index = index;
+    fn get_mem_index(&self, index: i64, mode: Mode) -> i64 {
+        let real_index;
         match mode {
-            0 => {
-                if self.memory.has_key(index) {
-                    real_index = self.memory[index];
+            Mode::POS => {
+                if self.memory.contains_key(&index) {
+                    real_index = self.memory[&index];
                 } else {
                     real_index = 0;
                 }
             }
-            1 => {
+            Mode::IMM => {
                 real_index = index;
             }
-            2 => {
-                real_index = index + self.relative_base;
+            Mode::REL => {
+                real_index = self.get_mem_index(index, Mode::POS) + self.relative_base;
             }
         }
         real_index
     }
 
-    fn get_mem(&self, index: i64, mode: usize) -> i64 {
-        let real_index = self.get_mem_index(index,mode);
-        if self.memory.has_key(real_index) {
-            self.memory[real_index]
-        } else { 0 }
+    fn get_mem(&self, index: i64, mode: Mode) -> i64 {
+        let real_index = self.get_mem_index(index, mode);
+        if self.memory.contains_key(&real_index) {
+            self.memory[&real_index]
+        } else {
+            0
+        }
     }
 
-    fn set_mem(&self, index: i64, mode: usize, value: i64) {
+    fn set_mem(&mut self, index: i64, mode: Mode, value: i64) {
         let real_index = self.get_mem_index(index, mode);
-        if self.memory.has_key(real_index) {
-            self.memory.get_mut(real_index) = value;
+        if self.memory.contains_key(&real_index) {
+            if let Some(x) = self.memory.get_mut(&real_index) {
+                *x = value;
+            }
         } else {
             self.memory.insert(real_index, value);
         }
@@ -90,8 +97,7 @@ impl IntCodeComputer {
     fn run(&mut self) -> bool {
         let mut result = false;
         loop {
-            // let mut prev_instr = 0;
-            let mut instructioncode: i64 = self.get_mem(self.pc + 0);
+            let mut instructioncode: i64 = self.get_mem(self.pc + 0, Mode::IMM);
             let opcode = instructioncode % 100;
 
             if opcode == 3 && self.input.len() == 0 {
@@ -103,165 +109,88 @@ impl IntCodeComputer {
             let mut modes: Vec<Mode> = vec![Mode::POS; 4];
             if instructioncode > 0 {
                 // apparently we need some different operation modes
-                let parammode1 = instructioncode % 1000;
-                if parammode1 > 0 {
-                    modes[0] = Mode::IMM;
-                }
-                instructioncode -= parammode1;
-
-                let parammode2 = instructioncode % 10000;
-                if parammode2 > 0 {
-                    modes[1] = Mode::IMM;
-                }
-                instructioncode -= parammode2;
-
-                let parammode3 = instructioncode % 10000;
-                if parammode3 > 0 {
-                    modes[2] = Mode::IMM;
-                }
-                instructioncode -= parammode3;
-
-                let parammode4 = instructioncode % 100000;
-                if parammode4 > 0 {
-                    modes[3] = Mode::IMM;
+                for i in 0usize..4 {
+                    let parammode = (instructioncode / (10 as i64).pow(i as u32 + 2)) % 10;
+                    match parammode {
+                        0 => {
+                            modes[i] = Mode::POS;
+                        }
+                        1 => {
+                            modes[i] = Mode::IMM;
+                        }
+                        2 => {
+                            modes[i] = Mode::REL;
+                        }
+                        _ => {}
+                    }
                 }
             }
 
-            let mut paramsize: i64 = 3;
+            let paramsize: i64;
 
-            let memaddr: Vec<usize> = if opcode == 1 || opcode == 2 || opcode == 7 || opcode == 8 {
-                paramsize = 3;
-                self.memory[self.pc + 1..self.pc + paramsize + 1]
-                    .iter()
-                    .filter_map(|s| Some(*s as usize))
-                    .collect()
-            } else if opcode == 3 || opcode == 4 {
-                paramsize = 1;
-                self.memory[self.pc + 1..self.pc + paramsize + 1]
-                    .iter()
-                    .filter_map(|s| Some(*s as usize))
-                    .collect()
-            } else if opcode == 5 || opcode == 6 {
-                paramsize = 2;
-                self.memory[self.pc + 1..self.pc + paramsize + 1]
-                    .iter()
-                    .filter_map(|s| Some(*s as usize))
-                    .collect()
-            } else {
-                vec![]
-            };
             let mut pc_modified = false;
             match opcode {
                 1 => {
-                    self.set_mem(self.pc+3, modes[2], 
-                        self.get_mem(self.pc+1, modes[0]) +
-                        self.get_mem(self.pc+2, modes[1]);
+                    paramsize = 3;
+                    self.set_mem(
+                        self.pc + 3,
+                        modes[2],
+                        self.get_mem(self.pc + 1, modes[0]) + self.get_mem(self.pc + 2, modes[1]),
                     );
                 }
                 2 => {
-                    let wpos = if modes[2] == Mode::POS {
-                        memaddr[2]
-                    } else {
-                        self.pc + 3
-                    };
-                    self.memory[wpos] = if modes[0] == Mode::POS {
-                        self.memory[memaddr[0]]
-                    } else {
-                        self.memory[self.pc + 1]
-                    } * if modes[1] == Mode::POS {
-                        self.memory[memaddr[1]]
-                    } else {
-                        self.memory[self.pc + 2]
-                    }
+                    paramsize = 3;
+                    self.set_mem(
+                        self.pc + 3,
+                        modes[2],
+                        self.get_mem(self.pc + 1, modes[0]) * self.get_mem(self.pc + 2, modes[1]),
+                    );
                 }
                 3 => {
+                    paramsize = 1;
                     if let Some(input) = self.get_input() {
-                        if modes[0] == Mode::POS {
-                            self.memory[memaddr[0]] = input;
-                        } else {
-                            self.memory[self.pc + 1] = input;
-                        }
+                        self.set_mem(self.pc + 1, modes[0], input);
                     }
                 }
                 4 => {
-                    let outval = if modes[0] == Mode::POS {
-                        self.memory[memaddr[0]]
-                    } else {
-                        self.memory[self.pc + 1]
-                    };
-                    self.add_output(outval);
+                    paramsize = 1;
+                    self.add_output(self.get_mem(self.pc + 1, modes[0]));
                 }
                 5 => {
+                    paramsize = 2;
                     // jump if true first paramter is non-zero ip is iset to parameter 2
-                    let val2test: i64 = if modes[0] == Mode::POS {
-                        self.memory[memaddr[0]]
-                    } else {
-                        self.memory[self.pc + 1]
-                    };
+                    let val2test = self.get_mem(self.pc + 1, modes[0]);
                     if val2test != 0 {
-                        let rpos = if modes[1] == Mode::POS {
-                            memaddr[1]
-                        } else {
-                            self.pc + 2
-                        };
-                        self.pc = self.memory[rpos as usize] as usize;
+                        self.pc = self.get_mem(self.pc + 2, modes[1]);
                         pc_modified = true;
                     }
                 }
                 6 => {
+                    paramsize = 2;
                     // jump if false first paramter is non-zero ip is iset to parameter 2
-                    let val2test: i64 = if modes[0] == Mode::POS {
-                        self.memory[memaddr[0]]
-                    } else {
-                        self.memory[self.pc + 1]
-                    };
+                    let val2test = self.get_mem(self.pc + 1, modes[0]);
                     if val2test == 0 {
-                        let rpos = if modes[1] == Mode::POS {
-                            memaddr[1]
-                        } else {
-                            self.pc + 2
-                        };
-                        self.pc = self.memory[rpos as usize] as usize;
+                        self.pc = self.get_mem(self.pc + 2, modes[1]);
                         pc_modified = true;
                     }
                 }
                 7 => {
+                    paramsize = 3;
                     // less than p1 < p2 => 1 in p3 else 0 in p3
-                    let val1test: i64 = if modes[0] == Mode::POS {
-                        self.memory[memaddr[0]]
-                    } else {
-                        self.memory[self.pc + 1]
-                    };
-                    let val2test: i64 = if modes[1] == Mode::POS {
-                        self.memory[memaddr[1]]
-                    } else {
-                        self.memory[self.pc + 2]
-                    };
-                    let wpos = if modes[2] == Mode::POS {
-                        memaddr[2]
-                    } else {
-                        self.pc + 3
-                    };
-                    self.memory[wpos as usize] = (val1test < val2test) as i64;
+                    let val1test = self.get_mem(self.pc + 1, modes[0]);
+                    let val2test = self.get_mem(self.pc + 2, modes[1]);
+                    self.set_mem(self.pc + 3, modes[2], (val1test < val2test) as i64);
                 }
                 8 => {
+                    paramsize = 3;
                     // less than p1 == p2 => 1 in p3 else 0 in p3
-                    let val1test: i64 = if modes[0] == Mode::POS {
-                        self.memory[memaddr[0]]
-                    } else {
-                        self.memory[self.pc + 1]
-                    };
-                    let val2test: i64 = if modes[1] == Mode::POS {
-                        self.memory[memaddr[1]]
-                    } else {
-                        self.memory[self.pc + 2]
-                    };
-                    let wpos = if modes[2] == Mode::POS {
-                        memaddr[2]
-                    } else {
-                        self.pc + 3
-                    };
-                    self.memory[wpos as usize] = (val1test == val2test) as i64;
+                    let val1test = self.get_mem(self.pc + 1, modes[0]);
+                    let val2test = self.get_mem(self.pc + 2, modes[1]);
+                    self.set_mem(self.pc + 3, modes[2], (val1test == val2test) as i64);
+                }
+                9 => {
+                    paramsize = 1;
+                    self.relative_base += self.get_mem(self.pc + 1, modes[0]);
                 }
                 99 => {
                     result = true;
@@ -271,7 +200,6 @@ impl IntCodeComputer {
                     break;
                 }
             }
-            // prev_instr = self.pc;
 
             if !pc_modified {
                 self.pc += paramsize + 1;
@@ -290,26 +218,42 @@ pub fn run() {
     let input = tools::get_input(String::from(input_file));
     let line = &input[0];
     let command_strings: Vec<&str> = line.split(",").collect();
-    let commands: Vec<i64> = command_strings
+    let mut commands: BTreeMap<i64, i64> = BTreeMap::new();
+    command_strings
         .iter()
         .filter_map(|s| s.parse::<i64>().ok())
-        .collect();
+        .enumerate()
+        .for_each(|(i, c)| {
+            commands.insert(i as i64, c);
+        });
 
     let start1 = Instant::now();
 
+    let mut computer1 = IntCodeComputer::new(&commands);
+    computer1.add_input(1);
+
+    let mut res1 = vec![];
+    if computer1.run() {
+        while let Some(res) = computer1.get_output() {
+            res1.push(res);
+        }
+    }
+
     let after1 = Instant::now();
     println!(
-        "Part 1: {}, in {:?}",
-        0,
+        "Part 1: {:?}, in {:?}",
+        res1[0],
         after1.duration_since(start1)
     );
 
     let start2 = Instant::now();
 
+    let mut computer2 = IntCodeComputer::new(&commands);
+    computer2.add_input(2);
+    computer2.run();
+
+    let res2: i64 = computer2.get_output().unwrap();
+
     let after2 = Instant::now();
-    println!(
-        "Part 2: {}, in {:?}",
-        0,
-        after2.duration_since(start2)
-    );
+    println!("Part 2: {}, in {:?}", res2, after2.duration_since(start2));
 }
