b69d0bd250529fe7118e6052277b036f993d19fd
Arno Kamphuis <ik@arnokamphuis.nl>
2020-12-02 12:05:06 +0100

    Initial day 3

---
+++ 2020/src/day03_20.rs
@@ -0,0 +1,26 @@
+use std::time::{Instant};
+use super::tools;
+
+#[allow(dead_code)]
+pub fn run() {
+    println!("Day 03 of 2020");
+
+    let start0 = Instant::now();
+
+    let input_file = "./input/day03_20_test.txt";
+    // let input_file = "./input/day03_20_real.txt";
+    let input = tools::get_input(String::from(input_file));
+
+    let after0 = Instant::now();
+    println!("Init in {:?}", after0.duration_since(start0));
+
+    let start1 = Instant::now();
+
+    let after1 = Instant::now();
+    println!("Part 1: {}, in {:?}", 0, after1.duration_since(start1));
+
+    let start2 = Instant::now();
+
+    let after2 = Instant::now();
+    println!("Part 2: {}, in {:?}", 0, after2.duration_since(start2));
+}
--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -1,8 +1,10 @@
 mod day01_20;
 mod day02_20;
+mod day03_20;
 mod tools;
 
 fn main() {
     // day01_20::run();
-    day02_20::run();
+    // day02_20::run();
+    day03_20::run();
 }
