c8d90e5e7b17074b78ea16837986e35aae7447c4
Arno Kamphuis <ik@arnokamphuis.nl>
2020-12-04 19:01:56 +0100

    Preparation day 5 of 2020

--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -2,11 +2,13 @@ mod day01_20;
 mod day02_20;
 mod day03_20;
 mod day04_20;
+mod day05_20;
 mod tools;
 
 fn main() {
     // day01_20::run();
     // day02_20::run();
     // day03_20::run();
-    day04_20::run();
+    // day04_20::run();
+    day05_20::run();
 }
