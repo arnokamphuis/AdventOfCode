793e7cbc123d883f0e06a52c99fa3a948ae45d7e
Arno Kamphuis <ik@arnokamphuis.nl>
2020-12-05 10:08:45 +0100

    Preparation day 6 of 2020

---
+++ 2020/src/day06_20.rs
@@ -0,0 +1,26 @@
+use super::tools;
+use std::time::Instant;
+
+#[allow(dead_code)]
+pub fn run() {
+    println!("Day 06 of 2020");
+
+    let start0 = Instant::now();
+
+    let input_file: &str = "./input/day06_20_test.txt";
+    // let input_file: &str = "./input/day06_20_real.txt";
+    let input = tools::get_input(String::from(input_file));
+
+    let after0 = Instant::now();
+    println!("Init in {:?}", after0.duration_since(start0));
+
+    let start1 = Instant::now();
+
+    let after1 = Instant::now();
+    println!("Part 1: {}, in {:?}", 0, after1.duration_since(start1));
+
+    let start2 = Instant::now();
+
+    let after2 = Instant::now();
+    println!("Part 2: {}, in {:?}", 0, after2.duration_since(start2));
+}
