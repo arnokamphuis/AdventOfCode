39b71c114636c34e5a411f977d7f40fca9a5067f
Arno Kamphuis <ik@arnokamphuis.nl>
2020-12-06 09:55:40 +0100

    Added command line arguments for 2020 to run a specific day

--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -1,3 +1,5 @@
+use std::env;
+
 mod day01_20;
 mod day02_20;
 mod day03_20;
@@ -8,10 +10,19 @@ mod passport;
 mod tools;
 
 fn main() {
-    // day01_20::run();
-    // day02_20::run();
-    // day03_20::run();
-    // day04_20::run();
-    // day05_20::run();
-    day06_20::run();
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        println!("Please specify the day to run!");
+        return
+    }
+
+    match args[1].parse::<usize>() {
+        Ok(1) => day01_20::run(),
+        Ok(2) => day02_20::run(),
+        Ok(3) => day03_20::run(),
+        Ok(4) => day04_20::run(),
+        Ok(5) => day05_20::run(),
+        Ok(6) => day06_20::run(),
+        _ => println!("Day {} is not available.", args[1])
+    }
 }
