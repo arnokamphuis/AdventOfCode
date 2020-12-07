8d3b7ee0e880cbca1ca5734e68eeb397a2adba6b
Arno Kamphuis <ik@arnokamphuis.nl>
2020-12-07 07:43:57 +0100

    Finished day 7 of 2020

--- 2020/src/day07_20.rs
+++ 2020/src/day07_20.rs
@@ -4,6 +4,18 @@ use std::collections::BTreeMap;
 use std::collections::BTreeSet;
 use queues::*;
 
+pub fn count_bags(current_bag: &String, factor: u128, rules: &BTreeMap<String, Vec<(usize, String)>>) -> u128 {
+    let mut amount = factor;
+
+    if let Some(active_rule) = rules.get(current_bag) {
+        for t in active_rule {
+            amount += count_bags(&t.1, factor * (t.0 as u128), rules);
+        }
+    }    
+
+    amount
+}
+
 #[allow(dead_code)]
 #[allow(unused_must_use)]
 pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
@@ -28,8 +40,7 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
         while w_iter.peek() != None {
             let amount_str = w_iter.next().unwrap();
             if amount_str.to_string() == "no" {
-                w_iter.next();
-                w_iter.next();
+                w_iter.next(); w_iter.next();
             } else {
                 let amount = amount_str.parse::<usize>().unwrap();
                 let mut otherbag = w_iter.next().unwrap().to_string();
@@ -40,33 +51,23 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
                 } else {
                     otherbag = String::from(&otherbag[0..otherbag.len()-1]);
                 }
-                // println!("{} {}", amount, otherbag);
                 rule.push( (amount, otherbag) );
             }
         }
         rules.insert(bag, rule.clone());
     }
 
+    let target = String::from("shinygoldbag");
+
     let after0 = Instant::now();
 
     let start1 = Instant::now();
 
-    let target = "shinygoldbag";
     let mut options: BTreeSet<String> = BTreeSet::new();
-    let mut q: Queue<String> = queue![];
-    for rule in &rules {
-        // println!("considering: {:?}",&rule);
-        for t in rule.1 {
-            if t.1 == target { 
-                options.insert(rule.0.clone()); 
-                q.add(rule.0.clone()); 
-            }
-        }
-    }
+    let mut q: Queue<String> = queue![target.clone()];
 
     while let Ok(bag) = q.remove() {
         for rule in &rules {
-            // println!("considering: {:?}",&rule);
             for t in rule.1 {
                 if t.1 == bag { 
                     options.insert(rule.0.clone()); 
@@ -76,16 +77,15 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
         }    
     }
 
-    // println!("{:?}",options);
-
-
     let after1 = Instant::now();
     if print_result { println!("Part 1: {}", options.len()); }
 
     let start2 = Instant::now();
 
+    let total = count_bags(&target, 1, &rules)-1; // subtract one because target is counted
+
     let after2 = Instant::now();
-    if print_result { println!("Part 2: {}", 0); }
+    if print_result { println!("Part 2: {}", total); }
 
     (after0.duration_since(start0).as_nanos(), after1.duration_since(start1).as_nanos(), after2.duration_since(start2).as_nanos())
 }
