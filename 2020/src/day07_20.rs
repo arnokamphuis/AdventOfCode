7f4be822e13474f98b6a7dfe32e958378b2a3a30
Arno Kamphuis <ik@arnokamphuis.nl>
2020-12-07 07:05:54 +0100

    Finished part 1 of day 2020

--- 2020/src/day07_20.rs
+++ 2020/src/day07_20.rs
@@ -1,19 +1,86 @@
 use super::tools;
 use std::time::Instant;
+use std::collections::BTreeMap;
+use std::collections::BTreeSet;
+use queues::*;
 
 #[allow(dead_code)]
+#[allow(unused_must_use)]
 pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     let start0 = Instant::now();
 
     let input_file: &str = if !real { "./input/day07_20_test.txt" } else { "./input/day07_20_real.txt" };
-    let _input = tools::get_input(String::from(input_file));
+    let input = tools::get_input(String::from(input_file));
+
+    let mut rules: BTreeMap<String, Vec<(usize, String)>> = BTreeMap::new();
+
+    for line in &input {
+        let mut rule: Vec<(usize, String)> = vec![];
+        let words: Vec<&str> = line.split(' ').collect();
+        let mut w_iter = words.iter().peekable();
+
+        let mut bag = w_iter.next().unwrap().to_string();
+        bag.push_str(w_iter.next().unwrap());
+        bag.push_str(w_iter.next().unwrap());
+        bag = String::from(&bag[0..bag.len()-1]);
+        w_iter.next();
+
+        while w_iter.peek() != None {
+            let amount_str = w_iter.next().unwrap();
+            if amount_str.to_string() == "no" {
+                w_iter.next();
+                w_iter.next();
+            } else {
+                let amount = amount_str.parse::<usize>().unwrap();
+                let mut otherbag = w_iter.next().unwrap().to_string();
+                otherbag.push_str(w_iter.next().unwrap());
+                otherbag.push_str(w_iter.next().unwrap());
+                if amount > 1 {
+                    otherbag = String::from(&otherbag[0..otherbag.len()-2]);
+                } else {
+                    otherbag = String::from(&otherbag[0..otherbag.len()-1]);
+                }
+                // println!("{} {}", amount, otherbag);
+                rule.push( (amount, otherbag) );
+            }
+        }
+        rules.insert(bag, rule.clone());
+    }
 
     let after0 = Instant::now();
 
     let start1 = Instant::now();
 
+    let target = "shinygoldbag";
+    let mut options: BTreeSet<String> = BTreeSet::new();
+    let mut q: Queue<String> = queue![];
+    for rule in &rules {
+        // println!("considering: {:?}",&rule);
+        for t in rule.1 {
+            if t.1 == target { 
+                options.insert(rule.0.clone()); 
+                q.add(rule.0.clone()); 
+            }
+        }
+    }
+
+    while let Ok(bag) = q.remove() {
+        for rule in &rules {
+            // println!("considering: {:?}",&rule);
+            for t in rule.1 {
+                if t.1 == bag { 
+                    options.insert(rule.0.clone()); 
+                    q.add(rule.0.clone()); 
+                }
+            }
+        }    
+    }
+
+    // println!("{:?}",options);
+
+
     let after1 = Instant::now();
-    if print_result { println!("Part 1: {}", 0); }
+    if print_result { println!("Part 1: {}", options.len()); }
 
     let start2 = Instant::now();
 
