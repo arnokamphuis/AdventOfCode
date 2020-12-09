33fb1dbed38dd50a91a9a71c0afbbe5e9f095737
Arno Kamphuis <ik@arnokamphuis.nl>
2020-12-09 18:13:21 +0100

    Finished day 9 of 2020

--- 2020/src/day09_20.rs
+++ 2020/src/day09_20.rs
@@ -1,6 +1,5 @@
 use super::tools;
 use std::time::Instant;
-use std::collections::BTreeSet;
 
 #[allow(dead_code)]
 pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
@@ -50,17 +49,27 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
 
     let start2 = Instant::now();
 
-
-    let mut seen: BTreeSet<Vec<i64>> = BTreeSet::new();
-    let mut res2 = 0;
-    if let Some(res) = filter_to_target(numbers.iter().map(|v| v).collect::<Vec<&i64>>(), contiguoustarget,contiguoustarget, vec![], &mut seen) {
-        // println!("{:?}",res);
-        res2 = res.iter().min().unwrap()+res.iter().max().unwrap();
+    let mut ans2 = 0;
+    for i in 0..numbers.len()-2 {
+        let mut sum: i64 = numbers[i] + numbers[i+1];
+        for j in i+2..numbers.len() {
+            sum += numbers[j];
+            if sum == contiguoustarget {
+                let set = numbers.iter().skip(i).take(j-i).map(|&v| v).collect::<Vec<i64>>();
+                ans2 = set.iter().min().unwrap() + set.iter().max().unwrap()
+            }
+            if sum > contiguoustarget {
+                break;
+            }
+        }
+        if ans2 > 0 {
+            break;
+        }
     }
 
     let after2 = Instant::now();
     if print_result {
-        println!("Part 2: {}", res2);
+        println!("Part 2: {}", ans2);
     }
 
     (
@@ -68,50 +77,4 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
         after1.duration_since(start1).as_nanos(),
         after2.duration_since(start2).as_nanos(),
     )
-}
-
-pub fn filter_to_target(set: Vec<&i64>, final_target: i64, target: i64, contains: Vec<i64>, seen: &mut BTreeSet<Vec<i64>> ) -> Option<Vec<i64>> {
-    // println!("     FILTER TO TARGET: {:?}, {}, {:?}", set,target,contains);
-    let smaller_set = set.iter().filter(|v| **v < &target ).map(|v| *v).collect::<Vec<&i64>>();
-    if smaller_set.len() == 0 {
-        return None;
-    }
-
-    let uptonow = contains.iter().sum::<i64>();
-    if contains.len() > 1 && set.iter().filter(|&&&v| uptonow+v==final_target).count() == 1 {
-        let mut new_contains = contains.clone();
-        new_contains.push(set.iter().filter(|&&&v| uptonow+v==final_target).fold(0, |acc, &&v| acc+v));
-        // println!("YES");
-        return Some(new_contains);
-    }
-
-    let small_diff_set: Vec<(&i64,i64)> = smaller_set.clone()
-        .iter()
-        .map(|v| (*v, target-**v) )
-        .collect();
-
-    // println!("--------------------------------------------------------------------------");
-    // println!("{:?}", small_diff_set);
-    for (s,t) in small_diff_set.iter() {
-        let new_target = *t;
-        let set: Vec<&i64> = smaller_set.iter().filter(|v|  **s != ***v ).map(|v| *v).collect();//***v < new_target &&
-        let mut new_contains = contains.clone();
-        new_contains.push(**s);
-
-        let mut pot_seen = new_contains.clone();
-        pot_seen.sort();
-        if seen.contains(&pot_seen) {
-
-        } else {
-            seen.insert(pot_seen);
-        
-            // println!("{} {} open: {:?}  close: {:?}", s, new_target, set, new_contains);
-
-            if let Some(res) = filter_to_target(set, final_target, new_target, new_contains, seen) {
-                return Some(res);
-            }
-        }
-    }
-    // println!("--------------------------------------------------------------------------");
-    return None
 }
