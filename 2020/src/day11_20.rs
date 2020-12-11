6ff4376e852c31e9910ae3ac689bbc99985611ca
Arno Kamphuis <arno.kamphuis@hu.nl>
2020-12-11 15:49:18 +0100

    Finished day 11 of 2020, needs cleaning/refactoring

--- 2020/src/day11_20.rs
+++ 2020/src/day11_20.rs
@@ -1,39 +1,112 @@
 use super::tools;
 use std::time::Instant;
-use std::collections::BTreeMap;
 
-type Grid = BTreeMap<(i32,i32), char>;
+type Grid = Vec<char>;
+
+pub fn set_seat(x: i32, y: i32, _: i32, width: i32, grid: &mut Grid, c: char) {
+    let index = (y * width + x) as usize;
+    grid[index] = c;
+}
+
+pub fn get_seat(x: i32, y: i32, height: i32, width: i32, grid: &Grid) -> char {
+    if !(x < 0 || x >= width || y < 0 || y >= height) {
+        let index = y * width + x;
+        if index >= 0 && index < grid.len() as i32 {
+            return grid[index as usize];
+        }
+    }
+    '.'
+}
+
+pub fn print_map(height: i32, width: i32, grid: &Grid) {
+    for y in 0..height {
+        for x in 0..width {
+            print!("{}", get_seat(x, y, width, height, &grid));
+        }
+        println!("");
+    }
+}
 
 pub fn step(grid: &Grid, height: i32, width: i32) -> (bool, Grid) {
     let mut not_changed = true;
     let mut new_grid = grid.clone();
     for y in 0..height {
         for x in 0..width {
-            if let Some(seat) = grid.get(&(x,y)) {
-                if *seat != '.' {
-                    let mut occ_count = 0;
-                    let mut free_count = 0;
-                    let tl; if let Some(c) = grid.get(&(x-1,y-1)) { tl = *c; if *c == '#' { occ_count += 1; } if *c == 'L' { free_count += 1; } }
-                    let tm; if let Some(c) = grid.get(&(x-1,y+0)) { tm = *c; if *c == '#' { occ_count += 1; } if *c == 'L' { free_count += 1; } }
-                    let tr; if let Some(c) = grid.get(&(x-1,y+1)) { tr = *c; if *c == '#' { occ_count += 1; } if *c == 'L' { free_count += 1; } }
-                    let ml; if let Some(c) = grid.get(&(x+0,y-1)) { ml = *c; if *c == '#' { occ_count += 1; } if *c == 'L' { free_count += 1; } }
-                    let mr; if let Some(c) = grid.get(&(x+0,y+1)) { mr = *c; if *c == '#' { occ_count += 1; } if *c == 'L' { free_count += 1; } }
-                    let bl; if let Some(c) = grid.get(&(x+1,y-1)) { bl = *c; if *c == '#' { occ_count += 1; } if *c == 'L' { free_count += 1; } }
-                    let bm; if let Some(c) = grid.get(&(x+1,y+0)) { bm = *c; if *c == '#' { occ_count += 1; } if *c == 'L' { free_count += 1; } }
-                    let br; if let Some(c) = grid.get(&(x+1,y+1)) { br = *c; if *c == '#' { occ_count += 1; } if *c == 'L' { free_count += 1; } }
-                    if *seat == 'L' && occ_count == 0 {
-                        if let Some(e) = new_grid.get_mut(&(x,y)) {
-                            *e = '#';
-                            not_changed = false;
+            let mut occ_count = 0;
+            for dy in -1..2 {
+                for dx in -1..2 {
+                    if !(dx == 0 && dy == 0) {
+                        let next_seat = get_seat(x + dx, y + dy, height, width, grid);
+                        if next_seat == '#' {
+                            occ_count += 1;
                         }
-                    }   
-                    if *seat == '#' && free_count >= 4 {
-                        if let Some(e) = new_grid.get_mut(&(x,y)) {
-                            *e = 'L';
-                            not_changed = false;
-                        }
-                    }   
+                    }
                 }
+            }
+
+            let seat = get_seat(x, y, height, width, grid);
+
+            if seat == 'L' && occ_count == 0 {
+                set_seat(x, y, height, width, &mut new_grid, '#');
+                not_changed = false;
+            }
+            if seat == '#' && occ_count >= 4 {
+                set_seat(x, y, height, width, &mut new_grid, 'L');
+                not_changed = false;
+            }
+        }
+    }
+    (not_changed, new_grid)
+}
+
+pub fn step2(grid: &Grid, height: i32, width: i32) -> (bool, Grid) {
+    let mut not_changed = true;
+    let mut new_grid = grid.clone();
+    for y in 0..height {
+        for x in 0..width {
+            let mut occ_count = 0;
+            for dy in -1..2 {
+                for dx in -1..2 {
+                    if !(dx == 0 && dy == 0) {
+                        let mut step_count = 1;
+                        loop {
+                            if x + step_count * dx < 0
+                                || x + step_count * dx >= width
+                                || y + step_count * dy < 0
+                                || y + step_count * dy >= height
+                            {
+                                break;
+                            }
+
+                            let next_seat = get_seat(
+                                x + step_count * dx,
+                                y + step_count * dy,
+                                height,
+                                width,
+                                grid,
+                            );
+
+                            if next_seat != '.' {
+                                if next_seat == '#' {
+                                    occ_count += 1;
+                                }
+                                break;
+                            }
+                            step_count += 1;
+                        }
+                    }
+                }
+            }
+
+            let seat = get_seat(x, y, height, width, grid);
+
+            if seat == 'L' && occ_count == 0 {
+                set_seat(x, y, height, width, &mut new_grid, '#');
+                not_changed = false;
+            }
+            if seat == '#' && occ_count >= 5 {
+                set_seat(x, y, height, width, &mut new_grid, 'L');
+                not_changed = false;
             }
         }
     }
@@ -53,49 +126,66 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     let height = input.len() as i32;
     let width = input[0].len() as i32;
 
-    let mut grid: Grid = BTreeMap::new();
-    let mut x = 0;
-    let mut y = 0;
+    let mut grid: Grid = vec![];
 
     for line in input {
-        x = 0;
         for c in line.chars() {
-            grid.insert((x,y), c);
-            x += 1;
+            grid.push(c);
         }
-        y += 1;
     }
 
-    println!("{:?}", grid);
+    // print_map(height, width, &grid);
+
     let after0 = Instant::now();
 
     let start1 = Instant::now();
 
     let mut round_count = 1;
-    let mut result = step(&grid,height, width);
-
-    for y in &grid {
-        println!("{:?}", y);
-    }
+    let mut result = step(&grid, height, width);
+    // println!("");
+    // print_map(height, width, &result.1);
 
     while !result.0 {
         result = step(&result.1, height, width);
+        // println!("");
+        // print_map(height, width, &result.1);
         round_count += 1;
     }
 
-    println!("round count {}", round_count);
+    let res1 = result
+        .1
+        .iter()
+        .fold(0, |acc, v| acc + if *v == '#' { 1 } else { 0 });
 
+    println!("round count {}", round_count);
 
     let after1 = Instant::now();
     if print_result {
-        println!("Part 1: {}", 0);
+        println!("Part 1: {}", res1);
     }
 
     let start2 = Instant::now();
 
+    round_count = 1;
+    result = step2(&grid, height, width);
+    // println!("");
+    // print_map(height, width, &result.1);
+
+    while !result.0 {
+        result = step2(&result.1, height, width);
+        // println!("");
+        // print_map(height, width, &result.1);
+        round_count += 1;
+    }
+
+    let res2 = result
+        .1
+        .iter()
+        .fold(0, |acc, v| acc + if *v == '#' { 1 } else { 0 });
+
     let after2 = Instant::now();
     if print_result {
-        println!("Part 2: {}", 0);
+        println!("Part 2: {}", res2);
     }
 
     (
