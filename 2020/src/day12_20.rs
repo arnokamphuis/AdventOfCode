0b0233a3ba8500a56d9d9809dd9c810aa0386403
Arno Kamphuis <ik@arnokamphuis.nl>
2020-12-12 18:57:50 +0100

    Added the video of day 12 part 2

--- 2020/src/day12_20.rs
+++ 2020/src/day12_20.rs
@@ -14,6 +14,7 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     let input = tools::get_input(String::from(input_file));
 
     // let mut path1: Vec<(i32,i32)> = vec![];
+    // let mut path2: Vec<(i32,i32)> = vec![];
 
     let mut commands: Vec<(&str, i32)> = vec![];
     for line in &input {
@@ -136,6 +137,8 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
         }
     };
 
+    // path2.push((0,0));
+
     let res2 = commands
         .iter()
         .fold(((0,0),(10,1)), |res, &step| {
@@ -150,10 +153,60 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
             if step.0 == "L" || step.0 == "R" {
                 new_pos = ((res.0.0, res.0.1), rotate2(res.1, step.0, step.1) );
             }
+            // path2.push(new_pos.0);
             new_pos
         });
 
     let after2 = Instant::now();
+
+    // let minimums = path2.iter().fold((10000,10000), | acc, v| {
+    //     (std::cmp::min(acc.0, v.0), std::cmp::min(acc.1,v.1))
+    // });
+    // let maximums = path2.iter().fold((-10000,-10000), | acc, v| {
+    //     (std::cmp::max(acc.0, v.0), std::cmp::max(acc.1,v.1))
+    // });
+
+    // let size = (maximums.0 - minimums.0, maximums.1 - minimums.1);
+    // println!("{:?} {:?} {:?} ", size, minimums, maximums);
+
+    // let smaller_factor = 100;
+
+    // let mut path_image = Image::new(1+(size.0 as usize +1)/smaller_factor, 1+(size.1 as usize +1)/smaller_factor, 2);
+    // let mut counter = 0;
+    // path_image.save_png(&format!("images/path{:010}.png",counter));
+
+    // commands
+    //     .iter()
+    //     .fold(((0,0),(10,1)), |res, &step| {
+    //         // ship is .0 , waypoint is .1
+    //         let cur_step = step2(step.0, res.1, step.1);
+    //         let mut new_pos = 
+    //             (
+    //                 (res.0.0+cur_step.0.0,res.0.1+cur_step.0.1),
+    //                 (res.1.0+cur_step.1.0,res.1.1+cur_step.1.1)
+    //             );
+
+    //         let steps = ((cur_step.0.0 * cur_step.0.0 + cur_step.0.1 * cur_step.0.1)  as f64).sqrt().floor() as i32;
+    //         if steps > 0 {
+    //             for i in 0..steps+1 {
+    //                 let temp_pos = (res.0.0 + (i * cur_step.0.0)/steps, res.0.1 + (i*cur_step.0.1)/steps);
+    //                 path_image.set_pixel((temp_pos.0 - minimums.0) as usize / smaller_factor, (temp_pos.1 - minimums.1) as usize/ smaller_factor, (255,0,0,255));
+    //                 if counter % 1024 == 0 {
+    //                     path_image.save_png(&format!("images/path{:010}.png",counter/1024));
+    //                 }
+    //                 counter+=1;                    
+    //             }
+    //         }
+
+    //         if step.0 == "L" || step.0 == "R" {
+    //             new_pos = ((res.0.0, res.0.1), rotate2(res.1, step.0, step.1) );
+    //         }
+
+    //         path2.push(new_pos.0);
+
+    //         new_pos
+    //     });
+
     if print_result {
         println!("Part 2: {}",  res2.0.0.abs() + res2.0.1.abs());
     }
