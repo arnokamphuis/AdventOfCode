6ba06b725e771ab8674eba86be76ff60ae19d12e
Arno Kamphuis <ik@arnokamphuis.nl>
2020-12-13 09:18:11 +0100

    Finished day 13 of 2020

--- 2020/src/day13_20.rs
+++ 2020/src/day13_20.rs
@@ -1,5 +1,23 @@
 use super::tools;
 use std::time::Instant;
+use modinverse::modinverse;
+
+fn chinese_remainder(constraints: &Vec<(i64,i64)>) -> Option<i64> {
+    // product of the modulos
+    let product = constraints.iter().map(|&v| v.1).product::<i64>(); 
+ 
+    // x = SUM a_i M_i N_i, where a_i is the remainder, N_i is p as described below, and M_i should be the modular inverse
+    // Bezout theorem: find integers u_i and v_i such as u_i n_i + v_i ^n_i = 1. Here, v_i is the modular inverse of ^n_i modulo n_i.
+    // See https://www.dcode.fr/chinese-remainder for a more elaborate explanation
+    let mut x = 0;
+    for (remainder, modulus) in constraints {
+        // p is product but one, a stated in the wiki page as N_i = N/n_i
+        let p: i64 = product / modulus;
+        x += remainder * modinverse(p, *modulus)? * p
+    }
+    // return the remainder of the sum by the product
+    Some(x % product)
+}
 
 #[allow(dead_code)]
 pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
@@ -10,22 +28,44 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     } else {
         "./input/day13_20_real.txt"
     };
-    let _input = tools::get_input(String::from(input_file));
+    let input = tools::get_input(String::from(input_file));
+
+    let departure_time = input[0].parse::<usize>().unwrap();
+    let numbers = input[1].split(',').map(|s| if let Ok(v) = s.parse::<usize>() {v} else {0}).filter(|&v| v > 0).collect::<Vec<usize>>();
+
+    // Create remainders and modulos for Chinese Remainder Theory 
+    //   see https://en.wikipedia.org/wiki/Chinese_remainder_theorem
+    let constraints = input[1].split(',').enumerate()
+        .map(|s| if let Ok(v) = s.1.parse::<i64>() {
+            ( (v-(s.0 as i64 % v))%v , v ) 
+        } else {
+            (-1,-1)
+        }).filter(|&v| v.0 > -1 && v.1 > -1)
+        .map(|v| (v.0 as i64, v.1 as i64)).collect::<Vec<(i64,i64)>>();
 
     let after0 = Instant::now();
 
     let start1 = Instant::now();
 
+    let res1 = numbers.iter()
+        .map(|&v| (v, v - departure_time % v) )
+        .fold((0,1000), |acc, v| if v.1 < acc.1 { v } else { acc });
+
     let after1 = Instant::now();
     if print_result {
-        println!("Part 1: {}", 0);
+        println!("Part 1: {:?}", res1.0 * res1.1);
     }
 
     let start2 = Instant::now();
 
+    let mut res2 = 0;
+    if let Some(cr) = chinese_remainder(&constraints) {
+        res2 = cr;
+    }
+
     let after2 = Instant::now();
     if print_result {
-        println!("Part 2: {}", 0);
+        println!("Part 2: {}", res2);
     }
 
     (
