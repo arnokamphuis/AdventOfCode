e255bd6cf459872e61461bd2025bb73f204c5c41
Arno Kamphuis <ik@arnokamphuis.nl>
2020-12-14 08:42:59 +0100

    Finished day 14 of 2020

--- 2020/src/day14_20.rs
+++ 2020/src/day14_20.rs
@@ -1,5 +1,7 @@
 use super::tools;
 use std::time::Instant;
+use std::collections::BTreeMap;
+use regex::Regex;
 
 #[allow(dead_code)]
 pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
@@ -10,22 +12,107 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     } else {
         "./input/day14_20_real.txt"
     };
-    let _input = tools::get_input(String::from(input_file));
+    let input = tools::get_input(String::from(input_file));
+
+    let mut mem: BTreeMap<usize, u64> = BTreeMap::new();
+    let mut or_mask: u64 = 0;
+    let mut and_mask: u64 = 0;
 
     let after0 = Instant::now();
 
     let start1 = Instant::now();
 
+    let mask_regex = Regex::new(r"mask = ([X10]+)").expect("Invalid regex");
+    let mem_regex = Regex::new(r"mem\[(\d+)\] = (\d+)").expect("Invalid regex");
+
+    for line in &input {
+        if mask_regex.is_match(line) {
+            let captures = mask_regex.captures_iter(line);
+            for cap in captures {
+                let mask = cap.get(1).unwrap().as_str();
+                or_mask = 0;
+                and_mask = 0;
+                for c in mask.chars() {
+                    match c {
+                        'X' => { or_mask = (or_mask << 1) + 0; and_mask = (and_mask << 1) + 1; },
+                        '1' => { or_mask = (or_mask << 1) + 1; and_mask = (and_mask << 1) + 1; },
+                        '0' => { or_mask = (or_mask << 1) + 0; and_mask = (and_mask << 1) + 0; },
+                        _ => {}
+                    }
+                }
+            }
+        }
+        if mem_regex.is_match(line) {
+            let captures = mem_regex.captures_iter(line);
+            for cap in captures {
+                let address = cap.get(1).unwrap().as_str().parse::<usize>().unwrap();
+                let value = cap.get(2).unwrap().as_str().parse::<u64>().unwrap();
+
+                mem.entry(address).or_insert(0);
+                if let Some(m) = mem.get_mut(&address) {
+                    *m = (value & and_mask) | or_mask;
+                }
+            }
+        }
+    }
+
+    let res1 = mem.iter().fold(0, |acc, v| acc + *v.1 );
+
     let after1 = Instant::now();
     if print_result {
-        println!("Part 1: {}", 0);
+        println!("Part 1: {}", res1);
     }
 
     let start2 = Instant::now();
 
+    mem.clear();
+    let mut x_pos = vec![];
+    for line in &input {
+        if mask_regex.is_match(line) {
+            x_pos = vec![];
+            let captures = mask_regex.captures_iter(line);
+            for cap in captures {
+                let mask = cap.get(1).unwrap().as_str();
+                or_mask = 0;
+                for (i,c) in mask.chars().enumerate() {
+                    match c {
+                        'X' => { or_mask = (or_mask << 1) + 0; x_pos.push(35-i) },
+                        '1' => { or_mask = (or_mask << 1) + 1 },
+                        '0' => { or_mask = (or_mask << 1) + 0 },
+                        _ => {}
+                    }
+                }
+            }
+        }
+        if mem_regex.is_match(line) {
+            let captures = mem_regex.captures_iter(line);
+            for cap in captures {
+                let address = cap.get(1).unwrap().as_str().parse::<u64>().unwrap();
+                let value = cap.get(2).unwrap().as_str().parse::<u64>().unwrap();
+
+                for t in 0..(1 << x_pos.len()) {
+                    let mut new_addr = address | or_mask;
+                    let mut tb: usize = 0;
+                    for b in &x_pos {
+                        if t & (1 << tb) > 0 {
+                            new_addr |= 1 << b;
+                        } else {
+                            new_addr &= !(1 << b);
+                        }
+                        tb += 1;
+                    }
+                    mem.entry(new_addr as usize).or_insert(0);
+                    if let Some(m) = mem.get_mut(&(new_addr as usize)) { *m = value; }
+                }
+            }
+        }
+    }
+
+    let res2 = mem.iter().fold(0, |acc, v| acc + *v.1 );
+
     let after2 = Instant::now();
     if print_result {
-        println!("Part 2: {}", 0);
+        println!("Part 2: {}", res2);
     }
 
     (
