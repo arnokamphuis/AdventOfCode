7b3c5a430e3183a629b6f187a56bf91f82d87eea
Arno Kamphuis <ik@arnokamphuis.nl>
2020-12-15 14:20:26 +0100

    Changed the code of the performance checker

--- 2020/src/main.rs
+++ 2020/src/main.rs
@@ -19,22 +19,22 @@ mod passport;
 mod tools;
 
 fn main() {
-    let days: Vec<(&str, fn(bool, bool) -> (u128, u128, u128))> = vec![
-        ("Day 01 of 2020", day01_20::run),
-        ("Day 02 of 2020", day02_20::run),
-        ("Day 03 of 2020", day03_20::run),
-        ("Day 04 of 2020", day04_20::run),
-        ("Day 05 of 2020", day05_20::run),
-        ("Day 06 of 2020", day06_20::run),
-        ("Day 07 of 2020", day07_20::run),
-        ("Day 08 of 2020", day08_20::run),
-        ("Day 09 of 2020", day09_20::run),
-        ("Day 10 of 2020", day10_20::run),
-        ("Day 11 of 2020", day11_20::run),
-        ("Day 12 of 2020", day12_20::run),
-        ("Day 13 of 2020", day13_20::run),
-        ("Day 14 of 2020", day14_20::run),
-        ("Day 15 of 2020", day15_20::run),
+    let days: Vec<(&str, fn(bool, bool) -> (u128, u128, u128), usize)> = vec![
+        ("Day 01 of 2020", day01_20::run, 500),
+        ("Day 02 of 2020", day02_20::run, 500),
+        ("Day 03 of 2020", day03_20::run, 500),
+        ("Day 04 of 2020", day04_20::run, 500),
+        ("Day 05 of 2020", day05_20::run, 500),
+        ("Day 06 of 2020", day06_20::run, 500),
+        ("Day 07 of 2020", day07_20::run, 500),
+        ("Day 08 of 2020", day08_20::run, 500),
+        ("Day 09 of 2020", day09_20::run, 500),
+        ("Day 10 of 2020", day10_20::run, 500),
+        ("Day 11 of 2020", day11_20::run, 100),
+        ("Day 12 of 2020", day12_20::run, 500),
+        ("Day 13 of 2020", day13_20::run, 500),
+        ("Day 14 of 2020", day14_20::run, 100),
+        ("Day 15 of 2020", day15_20::run, 4),
     ];
 
     let args: Vec<String> = env::args().collect();
@@ -46,21 +46,20 @@ fn main() {
     if let Ok(real) = args[2].parse::<bool>() {
         println!("-----------------------------------------------------------------------------------------------------------------------");
         if args[1] == "performance" {
-            let runs = 500u128;
-            println!("Running performance check using {} runs", runs);
+            println!("Running performance check");
             println!("");
-            for (name, day) in days.iter() {
+            for (name, day, runs) in days.iter() {
                 let mut timings: Vec<(u128, u128, u128)> = vec![];
-                for _ in 0..runs {
+                for _ in 0..*runs {
                     timings.push(day(real, false));
                 }
                 let total = timings.iter().fold((0, 0, 0), |acc, val| {
                     (acc.0 + val.0, acc.1 + val.1, acc.2 + val.2)
                 });
                 let mean = (
-                    total.0 as f64 / runs as f64,
-                    total.1 as f64 / runs as f64,
-                    total.2 as f64 / runs as f64,
+                    total.0 as f64 / *runs as f64,
+                    total.1 as f64 / *runs as f64,
+                    total.2 as f64 / *runs as f64,
                 );
                 let variance = timings
                     .iter()
@@ -75,9 +74,9 @@ fn main() {
                     })
                     .fold((0.0, 0.0, 0.0), |acc, val| {
                         (
-                            acc.0 + val.0 / (runs as f64),
-                            acc.1 + val.1 / (runs as f64),
-                            acc.2 + val.2 / (runs as f64),
+                            acc.0 + val.0 / (*runs as f64),
+                            acc.1 + val.1 / (*runs as f64),
+                            acc.2 + val.2 / (*runs as f64),
                         )
                     });
 
@@ -85,7 +84,7 @@ fn main() {
                     name, mean.0, variance.0.sqrt(), mean.1, variance.1.sqrt(), mean.2, variance.2.sqrt());
             }
         } else if args[1] == "all" {
-            for (name, day) in days.iter() {
+            for (name, day, _) in days.iter() {
                 let timing = day(real, false);
                 println!(
                     "{} - init: {:10} ns, p1: {:10} ns, p2: {:10} ns",
