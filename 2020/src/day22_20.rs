cc2f14f9a6b19d36496b57feded19e50d007d76f
Arno Kamphuis <ik@arnokamphuis.nl>
2020-12-22 12:03:48 +0100

    Some refactoring of day 22

--- 2020/src/day22_20.rs
+++ 2020/src/day22_20.rs
@@ -1,29 +1,34 @@
 use super::tools;
 use std::time::Instant;
-use std::collections::BTreeSet;
+use std::collections::HashSet;
 
-fn play_rc(s_p_deck: &Vec<usize>, s_o_deck: &Vec<usize>) -> (usize, Vec<usize>) {
-    let mut prev_p_decks: BTreeSet<Vec<usize>> = BTreeSet::new();
-    let mut prev_o_decks: BTreeSet<Vec<usize>> = BTreeSet::new();
+fn play_rc(s_p_deck: &Vec<usize>, s_o_deck: &Vec<usize>, part: usize) -> (usize, usize) {
+    let score = |deck: &Vec<usize>| {
+        deck.iter().enumerate().fold(0, |acc, (index, v)| acc + (deck.len() - index) * v )
+    };
+
+    let mut prev_p_deck: HashSet<Vec<usize>> = HashSet::new();
+    let mut prev_o_deck: HashSet<Vec<usize>> = HashSet::new();
 
     let mut p_deck = s_p_deck.clone();
     let mut o_deck = s_o_deck.clone();
 
-    while p_deck.len()!=0 && o_deck.len()!= 0 {
-        if prev_p_decks.contains(&p_deck) || prev_p_decks.contains(&o_deck) {
-            return (1, p_deck.clone());
-        } else {
-            prev_p_decks.insert(p_deck.clone());
-            prev_o_decks.insert(o_deck.clone());
+    while p_deck.len() != 0 && o_deck.len() != 0 {
+        if part == 2 {
+            if prev_p_deck.contains(&p_deck) || prev_o_deck.contains(&o_deck) {
+                return (1, score(&p_deck));
+            } else {
+                prev_p_deck.insert(p_deck.clone());
+                prev_o_deck.insert(o_deck.clone());
+            }
         }
 
         let p = p_deck.remove(0);
         let o = o_deck.remove(0);
 
-        let mut win_by = 0;
-        if p <= p_deck.len() && o <= o_deck.len() {
-            let (winner, _) = play_rc(&p_deck[0..p].to_vec(), &o_deck[0..o].to_vec());
-            win_by = winner;
+        let win_by;
+        if part == 2 && p <= p_deck.len() && o <= o_deck.len() {
+            win_by = play_rc(&p_deck[0..p].to_vec(), &o_deck[0..o].to_vec(), part).0;
         } else {
             if p > o { win_by = 1 } else { win_by = 2 }
         }
@@ -32,9 +37,9 @@ fn play_rc(s_p_deck: &Vec<usize>, s_o_deck: &Vec<usize>) -> (usize, Vec<usize>) 
     }
 
     if p_deck.len() > 0 {
-        (1, p_deck.clone())
+        (1, score(&p_deck))
     } else {
-        (2, o_deck.clone())
+        (2, score(&o_deck))
     }
 }
 
@@ -53,32 +58,20 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     let mut o_deck: Vec<usize> = vec![];
 
 
-    let mut player = true;
+    let mut player_1 = true;
     for line in &input {
-        if line == "Player 1:" { player = true }
-        if line == "Player 2:" { player = false }
+        if line == "Player 2:" { player_1 = false }
 
         if let Ok(v) = line.parse::<usize>() {
-            if player { p_deck.push(v); } else { o_deck.push(v); }
+            if player_1 { p_deck.push(v); } else { o_deck.push(v); }
         }
     }
-
-    let orig_p_deck = p_deck.clone();
-    let orig_o_deck = o_deck.clone();
 
     let after0 = Instant::now();
 
     let start1 = Instant::now();
 
-    while p_deck.len()!=0 && o_deck.len()!= 0 {
-        let p = p_deck.remove(0);
-        let o = o_deck.remove(0);
-
-        if p > o { p_deck.push(p); p_deck.push(o) } else { o_deck.push(o); o_deck.push(p); }
-    }
-
-    let winning_deck:Vec<usize> = if o_deck.len()==0 { p_deck.clone() } else { o_deck.clone() };
-    let res1 = winning_deck.iter().enumerate().fold(0, |acc, v| acc + (winning_deck.len() - v.0) * v.1 );
+    let res1 = play_rc(&p_deck, &o_deck, 1).1;
 
     let after1 = Instant::now();
     if print_result {
@@ -87,7 +80,7 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
 
     let start2 = Instant::now();
 
-    let res2 = play_rc(&orig_p_deck, &orig_o_deck).1.iter().enumerate().fold(0, |acc, v| acc + (winning_deck.len() - v.0) * v.1 );
+    let res2 = play_rc(&p_deck, &o_deck, 2).1;
 
     let after2 = Instant::now();
     if print_result {
