906215084dff10c9c3563a353a8b9f8564677b59
Arno Kamphuis <ik@arnokamphuis.nl>
2021-12-05 10:53:53 +0100

    Finished day 5 of 2021

--- 2021/src/day05_21.rs
+++ 2021/src/day05_21.rs
@@ -12,20 +12,91 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     };
     let input = tools::get_input(String::from(input_file));
 
+    let mut xcoors: Vec<usize> = vec![];
+    let mut ycoors: Vec<usize> = vec![];
+    let lines: Vec<((usize, usize), (usize, usize))> = input
+        .iter()
+        .map(|line| {
+            let mut tokens = line.split(" -> ");
+            let mut fromtokens = tokens.next().unwrap().split(",");
+            let f1 = fromtokens.next().unwrap().parse::<usize>().unwrap();
+            let f2 = fromtokens.next().unwrap().parse::<usize>().unwrap();
+            let mut totokens = tokens.next().unwrap().split(",");
+            let t1 = totokens.next().unwrap().parse::<usize>().unwrap();
+            let t2 = totokens.next().unwrap().parse::<usize>().unwrap();
+            xcoors.push(f1);
+            xcoors.push(t1);
+            ycoors.push(f2);
+            ycoors.push(t2);
+            ((f1, f2), (t1, t2))
+        })
+        .collect();
+
+    let maxx = *xcoors.iter().max().unwrap();
+    let maxy = *ycoors.iter().max().unwrap();
+
+    let mut field: Vec<Vec<usize>> = vec![vec![0; maxy + 1]; maxx + 1];
+
+    let hvlines: Vec<((usize, usize), (usize, usize))> = lines
+        .iter()
+        .filter(|line| line.0 .0 == line.1 .0 || line.0 .1 == line.1 .1)
+        .map(|&l| l)
+        .collect();
+    let diaglines: Vec<((usize, usize), (usize, usize))> = lines
+        .iter()
+        .filter(|line| line.0 .0 != line.1 .0 && line.0 .1 != line.1 .1)
+        .map(|&l| l)
+        .collect();
+
     let after0 = Instant::now();
 
     let start1 = Instant::now();
 
+    for line in hvlines {
+        if line.0 .0 == line.1 .0 {
+            // vertical
+            let delta = line.0 .1 as i32 - line.1 .1 as i32;
+            let dir: i32 = if delta < 0 { -1 } else { 1 };
+            for d in 0i32..delta.abs() + 1 {
+                let x = line.1 .0;
+                let y = (line.1 .1 as i32 + d * dir) as usize;
+                field[x][y] += 1;
+            }
+        } else {
+            // horizonal
+            let delta = line.0 .0 as i32 - line.1 .0 as i32;
+            let dir: i32 = if delta < 0 { -1 } else { 1 };
+            for d in 0i32..delta.abs() + 1 {
+                let x = (line.1 .0 as i32 + d * dir) as usize;
+                let y = line.0 .1;
+                field[x][y] += 1;
+            }
+        }
+    }
+    let res1 = field.iter().flatten().filter(|&&v| v > 1).count();
+
     let after1 = Instant::now();
     if print_result {
-        println!("Part 1: {}", 0);
+        println!("Part 1: {}", res1);
     }
 
     let start2 = Instant::now();
 
+    for line in diaglines {
+        let delta = line.0 .0 as i32 - line.1 .0 as i32;
+        let dirx: i32 = if delta < 0 { -1 } else { 1 };
+        let diry: i32 = if line.0 .1 < line.1 .1 { -1 } else { 1 };
+        for d in 0i32..delta.abs() + 1 {
+            let x = (line.1 .0 as i32 + d * dirx) as usize;
+            let y = (line.1 .1 as i32 + d * diry) as usize;
+            field[x][y] += 1;
+        }
+    }
+    let res2 = field.iter().flatten().filter(|&&v| v > 1).count();
+
     let after2 = Instant::now();
     if print_result {
-        println!("Part 2: {}", 0);
+        println!("Part 2: {}", res2);
     }
 
     (
--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -56,7 +56,7 @@ fn main() {
         ("Day 02 of 2021", day02_21::run, 500),
         ("Day 03 of 2021", day03_21::run, 500),
         ("Day 04 of 2021", day04_21::run, 500),
-        // ("Day 05 of 2021", day05_21::run, 50),
+        ("Day 05 of 2021", day05_21::run, 50),
         // ("Day 06 of 2021", day06_21::run, 50),
         // ("Day 07 of 2021", day07_21::run, 50),
         // ("Day 08 of 2021", day08_21::run, 50),
