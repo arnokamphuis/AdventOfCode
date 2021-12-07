1308d846308b120a30aa7b203cb0fd2cb9f77e6f
Arno Kamphuis <ik@arnokamphuis.nl>
2021-12-07 07:23:15 +0100

    Finished day 7 of 2021

---
+++ 2021/src/day07_21.rs
@@ -0,0 +1,56 @@
+use super::tools;
+use std::time::Instant;
+
+#[allow(dead_code)]
+pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
+    let start0 = Instant::now();
+
+    let input_file: &str = if !real {
+        "./input/day07_21_test.txt"
+    } else {
+        "./input/day07_21_real.txt"
+    };
+    let input = tools::get_input(String::from(input_file));
+
+    let positions = input[0]
+        .split(",")
+        .map(|v| v.parse::<i32>().unwrap())
+        .collect::<Vec<i32>>();
+
+    let min = *positions.iter().min().unwrap();
+    let max = *positions.iter().max().unwrap();
+
+    let after0 = Instant::now();
+
+    let start1 = Instant::now();
+
+    let mut fuel_total: Vec<i32> = vec![0;(max-min+1).try_into().unwrap()];
+    for i in min..=max {
+        fuel_total[i as usize] = positions.iter().map(|&p| (i-p).abs() as i32).sum::<i32>();
+    }
+    let res1 = fuel_total.iter().min().unwrap();
+
+    let after1 = Instant::now();
+    if print_result {
+        println!("Part 1: {}", res1);
+    }
+
+    let start2 = Instant::now();
+
+    fuel_total = vec![0;(max-min+1).try_into().unwrap()];
+    for i in min..=max {
+        fuel_total[i as usize] = positions.iter().map(|&p| {let f = (i-p).abs(); (f*(f+1)/2) as i32}).sum::<i32>();
+    }
+    let res2 = fuel_total.iter().min().unwrap();
+
+    let after2 = Instant::now();
+    if print_result {
+        println!("Part 2: {}", res2);
+    }
+
+    (
+        after0.duration_since(start0).as_nanos(),
+        after1.duration_since(start1).as_nanos(),
+        after2.duration_since(start2).as_nanos(),
+    )
+}
