77505b511b72171cec6844ef82e6ec63e37aaa22
Arno Kamphuis <ik@arnokamphuis.nl>
2021-12-07 19:07:01 +0100

    Prep day 8 of 2021 done

--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -7,6 +7,7 @@ mod day04_21;
 mod day05_21;
 mod day06_21;
 mod day07_21;
+mod day08_21;
 mod tools;
 mod chars;
 
@@ -61,7 +62,7 @@ fn main() {
         ("Day 05 of 2021", day05_21::run, 500),
         ("Day 06 of 2021", day06_21::run, 500),
         ("Day 07 of 2021", day07_21::run, 500),
-        // ("Day 08 of 2021", day08_21::run, 50),
+        ("Day 08 of 2021", day08_21::run, 500),
         // ("Day 09 of 2021", day09_21::run, 50),
         // ("Day 10 of 2021", day10_21::run, 50),
         // ("Day 11 of 2021", day11_21::run, 50),
