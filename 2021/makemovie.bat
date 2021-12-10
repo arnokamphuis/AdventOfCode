dd8e7ae72adc42b03432f801802e79f18184be88
Arno Kamphuis <ik@arnokamphuis.nl>
2021-12-10 08:24:56 +0100

    Extra lines for making videos

--- 2021/makemovie.bat
+++ 2021/makemovie.bat
@@ -1,4 +1,6 @@
-ffmpeg -framerate 2 -i boards-%05d.png -s:v 1280x720 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p day04-part2-2fps.mp4
+ffmpeg -framerate 100 -i day09-%05d.png -s:v 720x720 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p day09-part2-100fps.mp4
 
 
 ffmpeg -i day05-10fps.mp4 -vf "fps=10,scale=720:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 day05-10fps.gif
+
+ffmpeg -i day09-part2-200fps.mp4 -vf "fps=200,scale=720:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 day09-part2-200fps.gif
