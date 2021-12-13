084ac90f26bde15a07532255e1757bdf820902a5
Arno Kamphuis <ik@arnokamphuis.nl>
2021-12-13 08:46:54 +0100

    Finished day 13 of 2021

--- 2021/src/day13_21.rs
+++ 2021/src/day13_21.rs
@@ -1,5 +1,6 @@
 use super::tools;
 use std::time::Instant;
+use std::collections::BTreeSet;
 
 #[allow(dead_code)]
 pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
@@ -12,16 +13,64 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     };
     let input = tools::get_input(String::from(input_file));
 
+    let mut pixels: BTreeSet<(i64,i64)> = BTreeSet::new();
+    let mut folds: Vec<(String, i64)> = vec![];
+
+    let mut lc = 0;
+    loop {
+        if input[lc] == "" { lc+=1; break; }
+        let coor = input[lc].split(",").map(|s| s.parse::<i64>().unwrap()).collect::<Vec<i64>>();
+        pixels.insert((coor[0], coor[1]));
+        lc += 1;
+    }
+
+    while lc < input.len() {
+        let axis = input[lc][11..].to_string();
+        let mut tokens = axis.split("=");
+        folds.push((tokens.next().unwrap().to_string().clone(), tokens.next().unwrap().parse::<i64>().unwrap())); 
+        lc += 1;
+    }
+
+    let fold = | pxls: &BTreeSet<(i64,i64)>, (axis, value): &(String, i64) | -> BTreeSet<(i64,i64)> {
+        pxls
+            .iter()
+            .map(|pxl| {
+                match axis.as_str() {
+                    "x" => {
+                        if pxl.0 > *value { return (2 * value - pxl.0, pxl.1); } else { return *pxl; }
+                    }
+                    "y" => {
+                        if pxl.1 > *value { return (pxl.0, 2 * value - pxl.1); } else { return *pxl; }
+                    }
+                    _ => panic!()
+                }
+            }).collect::<BTreeSet<(i64,i64)>>()
+    };
+    
     let after0 = Instant::now();
 
     let start1 = Instant::now();
 
+    pixels = fold(&pixels, &folds[0]);
+
+    let res1 = pixels.len();
+
     let after1 = Instant::now();
     if print_result {
-        println!("Part 1: {}", 0);
+        println!("Part 1: {}", res1);
     }
 
     let start2 = Instant::now();
+
+    for i in 1..folds.len() {
+        pixels = fold(&pixels, &folds[i]);        
+    }
+
+    if print_result {
+        for i in 0..6i64 { for j in 0..39i64 { 
+            print!("{}", if pixels.contains(&(j,i)) { "#" } else { " " });
+        } println!(""); }
+    }
 
     let after2 = Instant::now();
     if print_result {
