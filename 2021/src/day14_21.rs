a06c22b1faf76e82cbbede3e64f5f51179a79083
Arno Kamphuis <ik@arnokamphuis.nl>
2021-12-14 19:38:07 +0100

    Refactoring done for day 14 of 2021

--- 2021/src/day14_21.rs
+++ 2021/src/day14_21.rs
@@ -15,22 +15,15 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     let input = tools::get_input(String::from(input_file));
 
     let polymer: Vec<char> = input[0].chars().collect();
-    let mut rules: HashMap<Vec<char>, char> = HashMap::new();
-    let mut chars: BTreeSet<char> = BTreeSet::new();
-    let mut emptycounts: HashMap<(char, char), usize> = HashMap::new();
+    let last: char = polymer.iter().last().unwrap().clone();
 
+    let mut rules: HashMap<Vec<char>, char> = HashMap::new();
     input.iter().skip(2).for_each(|line| {
         let mut tokens = line.split(" -> ");
         let from = tokens.next().unwrap().chars().collect::<Vec<char>>();
         let to: char = tokens.next().unwrap().chars().next().unwrap();
-        chars.insert(from[0]);
-        chars.insert(from[1]);
-        chars.insert(to);
         rules.insert(from, to);
     });
-    let last: char = polymer.iter().last().unwrap().clone();
-
-    chars.iter().for_each(|c1| { chars.iter().for_each(|c2| { emptycounts.insert((*c1,*c2),0); }); });
 
     let count_letters = | counts: &HashMap<(char, char), usize> | -> HashMap<char, usize> {
         let mut char_counts = counts.iter().map(|c| (c.0 .0, c.1)).fold(
@@ -40,24 +33,27 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
         char_counts    
     };
 
+    let step = | counts: &HashMap<(char, char), usize> | -> HashMap<(char, char), usize> {
+        rules
+            .iter()
+            .fold(HashMap::new(), |mut newcounts, (pair, &newc)| {
+                if let Some(cc) = counts.get(&(pair[0], pair[1])) {
+                    *newcounts.entry((pair[0], newc)).or_insert(0) += cc;
+                    *newcounts.entry((newc, pair[1])).or_insert(0) += cc;
+                }
+                newcounts
+            })
+    };
+
     let after0 = Instant::now();
 
     let start1 = Instant::now();
 
-    let mut counts = polymer.windows(2).fold(emptycounts.clone(), |mut map, p| {
-        if let Some(mc) = map.get_mut(&(p[0], p[1])) { *mc += 1; } map
+    let mut counts = polymer.windows(2).fold(HashMap::new(), |mut map, p| {
+        *map.entry((p[0],p[1])).or_insert(0) += 1; map
     });
 
-    (0..10).for_each(|_| {
-        counts = rules
-            .iter()
-            .fold(emptycounts.clone(), |mut newcounts, (pair, &newc)| {
-                let cc = counts[&(pair[0], pair[1])];
-                *newcounts.get_mut(&(pair[0], newc)).unwrap() += cc;
-                *newcounts.get_mut(&(newc, pair[1])).unwrap() += cc;
-                newcounts
-            });
-    });
+    (0..10).for_each(|_| { counts = step(&counts); });
 
     let char_counts = count_letters(&counts);
     let res1 = char_counts.iter().map(|(_, c)| *c).max().unwrap()
@@ -70,16 +66,7 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
 
     let start2 = Instant::now();
 
-    (10..40).for_each(|_| {
-        counts = rules
-            .iter()
-            .fold(emptycounts.clone(), |mut newcounts, (pair, &newc)| {
-                let cc = counts[&(pair[0], pair[1])];
-                *newcounts.get_mut(&(pair[0], newc)).unwrap() += cc;
-                *newcounts.get_mut(&(newc, pair[1])).unwrap() += cc;
-                newcounts
-            });
-    });
+    (10..40).for_each(|_| { counts = step(&counts); });
 
     let char_counts = count_letters(&counts);
     let res2 = char_counts.iter().map(|(_, c)| *c).max().unwrap()
