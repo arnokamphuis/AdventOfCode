cfb58fc333e90eb7e42f7f028b0044f70e2e57f0
Arno Kamphuis <arno.kamphuis@hu.nl>
2021-12-22 19:34:38 +0100

    Finished day 22 of 2021 with ugly code

--- 2021/src/day22_21.rs
+++ 2021/src/day22_21.rs
@@ -34,7 +34,6 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
             coor_tokens.next().unwrap().parse::<i64>().unwrap()];
         actions.push((vec![x_range, y_range, z_range], onoff));
     });
-    // println!("{:?}",actions);
 
     let after0 = Instant::now();
 
@@ -59,21 +58,84 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
 
     let start2 = Instant::now();
 
-    let x_coords: Vec<i64> = actions.iter().map(|action| vec![action.0[0][0],action.0[0][1]]).collect::<Vec<Vec<i64>>>().iter().flatten().map(|v|*v).collect();
-    let y_coords: Vec<i64> = actions.iter().map(|action| vec![action.0[1][0],action.0[1][1]]).collect::<Vec<Vec<i64>>>().iter().flatten().map(|v|*v).collect();
-    let z_coords: Vec<i64> = actions.iter().map(|action| vec![action.0[2][0],action.0[2][1]]).collect::<Vec<Vec<i64>>>().iter().flatten().map(|v|*v).collect();
+    let mut x_coords: Vec<i64> = actions.iter().map(|action| vec![action.0[0][0],action.0[0][1]+1]).collect::<Vec<Vec<i64>>>().iter().flatten().map(|v|*v).collect();
+    let mut y_coords: Vec<i64> = actions.iter().map(|action| vec![action.0[1][0],action.0[1][1]+1]).collect::<Vec<Vec<i64>>>().iter().flatten().map(|v|*v).collect();
+    let mut z_coords: Vec<i64> = actions.iter().map(|action| vec![action.0[2][0],action.0[2][1]+1]).collect::<Vec<Vec<i64>>>().iter().flatten().map(|v|*v).collect();
 
     x_coords.sort();
     y_coords.sort();
     z_coords.sort();
 
-    
+    let mut index = 0;
+    loop { 
+        if x_coords[index+1] == x_coords[index] { x_coords.remove(index); } else { index += 1; }
+        if index+1 >= x_coords.len() { break; }
+    }
+    index = 0;
+    loop { 
+        if y_coords[index+1] == y_coords[index] { y_coords.remove(index); } else { index += 1; }
+        if index+1 >= y_coords.len() { break; }
+    }
+    index = 0;
+    loop { 
+        if z_coords[index+1] == z_coords[index] { z_coords.remove(index); } else { index += 1; }
+        if index+1 >= z_coords.len() { break; }
+    }
 
-    println!("{:?}", x_coords);
+    let mut x_compressed: HashMap<i64,i64> = HashMap::new();
+    let mut x_sizes: HashMap<i64,i64> = HashMap::new();
+    for i in 0..x_coords.len()-1 {
+        if x_coords[i+1] - x_coords[i] > 0 {
+            x_compressed.insert(x_coords[i], x_sizes.len() as i64);
+            x_sizes.insert(x_sizes.len() as i64, x_coords[i+1] - x_coords[i]);
+        }
+    }
+    x_compressed.insert(x_coords[x_coords.len()-1], x_sizes.len() as i64);
+    x_sizes.insert(x_sizes.len() as i64,0);
+
+    let mut y_compressed: HashMap<i64,i64> = HashMap::new();
+    let mut y_sizes: HashMap<i64,i64> = HashMap::new();
+    for i in 0..y_coords.len()-1 {
+        if y_coords[i+1] - y_coords[i] > 0 {
+            y_compressed.insert(y_coords[i], y_sizes.len() as i64);
+            y_sizes.insert(y_sizes.len() as i64, y_coords[i+1] - y_coords[i]);
+        }
+    }
+    y_compressed.insert(y_coords[y_coords.len()-1], y_sizes.len() as i64);
+    y_sizes.insert(y_sizes.len() as i64,0);
+
+    let mut z_compressed: HashMap<i64,i64> = HashMap::new();
+    let mut z_sizes: HashMap<i64,i64> = HashMap::new();
+    for i in 0..z_coords.len()-1 {
+        if z_coords[i+1] - z_coords[i] > 0 {
+            z_compressed.insert(z_coords[i], z_sizes.len() as i64);
+            z_sizes.insert(z_sizes.len() as i64, z_coords[i+1] - z_coords[i]);
+        }
+    }
+    z_compressed.insert(z_coords[z_coords.len()-1], z_sizes.len() as i64);
+    z_sizes.insert(z_sizes.len() as i64,0);
+
+    let mut field2: HashMap<[i64;3],bool> = HashMap::new();
+    actions.iter().for_each(|action| {
+        (x_compressed[&action.0[0][0]]..x_compressed[&(action.0[0][1]+1)]).for_each(|x| {
+            (y_compressed[&action.0[1][0]]..y_compressed[&(action.0[1][1]+1)]).for_each(|y| {
+                (z_compressed[&action.0[2][0]]..z_compressed[&(action.0[2][1]+1)]).for_each(|z| {
+                    if action.1 {
+                        *field2.entry([x,y,z]).or_insert(false) = true;                                
+                    } else {
+                        field2.remove(&[x,y,z]);
+                    }
+                });
+            });
+        });
+    });
+    let res2 = field2.iter().fold(0, |count, (pos, _)| {
+        count + x_sizes[&pos[0]] * y_sizes[&pos[1]] * z_sizes[&pos[2]]
+    });    
 
     let after2 = Instant::now();
     if print_result {
-        println!("Part 2: {}", 0);
+        println!("Part 2: {}", res2);
     }
 
     (
