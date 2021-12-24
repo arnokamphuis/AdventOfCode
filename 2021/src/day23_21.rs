de3198ae72bce6c53bafb19a11947c0a72b5bf2f
Arno Kamphuis <ik@arnokamphuis.nl>
2021-12-24 22:03:26 +0100

    Finally finished day 23 of 2021

--- 2021/src/day23_21.rs
+++ 2021/src/day23_21.rs
@@ -6,8 +6,130 @@ use std::collections::HashMap;
 
 #[derive(Clone, Eq, PartialEq)]
 struct State {
-    energy: u64,
-    positions: [char; 15],
+    energy : usize,
+    top    : [char; 11],
+    bottom : [[char;4]; 4],
+}
+
+impl State {
+
+    fn new() -> State {
+        State {
+            energy: 0,
+            top: ['.'; 11],
+            bottom: [ ['A'; 4], ['B'; 4], ['C'; 4], ['D'; 4] ]
+        }
+    }
+
+    fn cost(&self, c: char) -> usize {
+        match c {
+            'A' => {1},
+            'B' => {10},
+            'C' => {100},
+            'D' => {1000},
+            _ => {panic!();}
+        }
+    }
+
+    fn room_index(&self, c: char) -> usize {
+        (c as u32 - 'A' as u32) as usize
+    }
+
+    fn finished(&self) -> bool {
+        self.is_done('A') && self.is_done('B') && self.is_done('C') && self.is_done('D') 
+    }
+
+    fn is_empty(&self, c: char) -> bool {
+        self.bottom[self.room_index(c)].iter().all(|&ch| ch=='.')
+    }
+
+    fn get_first(&self, room: char) -> Option<(usize, char)> {
+        for (depth, c) in self.bottom[self.room_index(room)].iter().enumerate() {
+            if *c != '.' { return Some((depth, *c)); }
+        }
+        None
+    }
+
+    fn get_free_in_room(&self, room: char) -> Option<usize> {
+        if self.bottom[self.room_index(room)].iter().all(|&ch| ch == '.' || ch == room) {
+            for (depth, c) in self.bottom[self.room_index(room)].iter().enumerate() {
+                if *c != '.' { return Some(depth-1); }
+            }
+            return Some(3);
+        }
+        None
+    }
+
+    fn is_done(&self, c: char) -> bool {
+        self.bottom[self.room_index(c)].iter().all(|&ch| ch==c)
+    }
+
+    fn get_top_targets(&self) -> Vec<usize> {
+        [0, 1, 3, 5, 7, 9, 10].iter().filter(|&index| self.top[*index] == '.').map(|&v|v).collect::<Vec<usize>>()
+    }
+
+    fn get_bottom_targets(&self) -> Vec<char> {
+        ['A', 'B', 'C', 'D'].iter().filter(|&ch| self.is_empty(*ch)).map(|&c|c).collect::<Vec<char>>()
+    }
+
+    fn move_room_to_room(&mut self, from_room: char, to_room: char) -> Option<usize> {
+        if from_room == to_room { return None; }
+
+        let from_pos = 2 * (1 + self.room_index(from_room));
+        let to_pos   = 2 * (1 + self.room_index(to_room));
+        let left     = from_pos.min(to_pos);
+        let right    = from_pos.max(to_pos);
+
+        if self.top[left..=right].iter().all(|&c| c=='.') {
+            if let Some((from_depth, from_ch)) = self.get_first(from_room) {
+                if from_ch != to_room { return None; }
+
+                if let Some(to_depth) = self.get_free_in_room(to_room) {
+                    self.bottom[self.room_index(from_room)][from_depth] = '.';
+                    self.bottom[self.room_index(to_room)][to_depth] = from_ch;
+                    return Some((from_depth + to_depth + 2 * (right - left)) * self.cost(from_ch));
+                }
+            }
+        }
+
+        None
+    }
+
+    fn move_from_room(&mut self, room: char, to: usize) -> Option<usize> {
+        if self.top[to] != '.' { return None; }
+        let from_pos = 2 * (1 + self.room_index(room));
+        if let Some((depth,ch)) = self.get_first(room) {
+            if self.top[from_pos.min(to)+1..from_pos.max(to)].iter().all(|&c| c=='.') {
+                self.top[to] = ch;
+                self.bottom[self.room_index(room)][depth] = '.';
+                return Some( (1+depth + from_pos.max(to) - from_pos.min(to)) * self.cost(ch) );
+            }
+        }
+        None
+    }
+
+    fn move_to_room(&mut self, from: usize, room: char) -> Option<usize> {
+        if self.top[from] == '.' { return None; }
+
+        let movable = self.top[from];
+        if movable != room { return None; }
+
+        let to_pos = 2 * (1 + self.room_index(room));
+        if let Some(depth) = self.get_free_in_room(room) {
+            if self.top[to_pos.min(from)+1..to_pos.max(from)].iter().all(|&c| c=='.') {
+                let ch = self.top[from];
+                self.bottom[self.room_index(room)][depth] = ch;
+                self.top[from] = '.';
+                return Some( (1+depth + to_pos.max(from) - to_pos.min(from)) * self.cost(ch) );
+            }
+        }
+        None
+    }
+
+    fn get_top_amphipods(&self) -> Vec<(char, usize)> {
+        self.top.iter().enumerate().filter(|(_,&ch)| ch != '.').map(|(index,&ch)| (ch,index)).collect()
+    }
+
 }
 
 impl Ord for State {
@@ -24,174 +146,93 @@ impl PartialOrd for State {
 
 
 #[allow(dead_code)]
-pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
+pub fn run(_real: bool, print_result: bool) -> (u128, u128, u128) {
     let start0 = Instant::now();
-
-    // let input_file: &str = if !real {
-    //     "./input/day23_21_test.txt"
-    // } else {
-    //     "./input/day23_21_real.txt"
-    // };
-    // let input = tools::get_input(String::from(input_file));
 
     let after0 = Instant::now();
 
-    let mut start_state = ['.';15];
-    if !real {
-    // test
-        start_state[7]  = 'B'; start_state[8]  = 'A';
-        start_state[9]  = 'C'; start_state[10] = 'D';
-        start_state[11] = 'B'; start_state[12] = 'C';
-        start_state[13] = 'D'; start_state[14] = 'A';
-    } else {
-        start_state[7]  = 'D'; start_state[8]  = 'B';
-        start_state[9]  = 'C'; start_state[10] = 'C';
-        start_state[11] = 'A'; start_state[12] = 'D';
-        start_state[13] = 'B'; start_state[14] = 'A';
-    }
-
-    let mut goal_state = ['.';15];
-    // goal
-    goal_state[7]  = 'A'; goal_state[8]  = 'A';
-    goal_state[9]  = 'B'; goal_state[10] = 'B';
-    goal_state[11] = 'C'; goal_state[12] = 'C';
-    goal_state[13] = 'D'; goal_state[14] = 'D';
-
-    let finished = | s: &[char;15] | -> bool {
-        *s == goal_state
-    };
-
-    let swap = | s: &mut[char;15], from: usize, to: usize | {
-        let temp = s[from]; s[from] = s[to]; s[to] = temp;
-    };
-
-    let cost = | c: char | -> u64 {
-        match c {
-            'A' => {1},
-            'B' => {10},
-            'C' => {100},
-            'D' => {1000},
-            _ => {panic!();}
-        }
-    };
-
-    let allowed = | s: &[char;15], from: usize, to: usize | -> (u64, bool) {
-        if from == to { return (0, false); }
-        if s[from] == '.' || (from < 7 && to < 7)  { return (0, false); }
-        if s[to] != '.' { return (0, false); }
-
-        if to >= 7 {
-            if !((to-7)/2 == 0 && s[from] == 'A') { return (0, false); }
-            if !((to-7)/2 == 1 && s[from] == 'B') { return (0, false); }
-            if !((to-7)/2 == 2 && s[from] == 'C') { return (0, false); }
-            if !((to-7)/2 == 3 && s[from] == 'D') { return (0, false); }
-        }
-        
-        if from >= 7 && to >= 7 {
-            let from_bottom = (from - 7) % 2;
-            let to_bottom   = ( to  - 7) % 2;
-
-            // println!("FROM {} ({}) TO {} ({})", from, s[from], to, s[to]);
-
-            // println!("from_bottom {}  s[from-1] = {}", from_bottom, s[from-1]);
-            if from_bottom == 1 && s[from-1] != '.' { return (0, false); }
-
-            // println!("to_bottom {}  s[to-1] = {}", to_bottom, s[to-1]);
-            if to_bottom   == 1 && s[to-1]   != '.' { return (0, false); }
-
-            // println!("to_bottom {}  s[to+1] = {}  s[from] = {}", to_bottom, s[to+1], s[from]);
-            if to_bottom   == 0 && s[to+1] != s[from] { return (0, false); }
-
-            let from_room = (from - 7)/2;
-            let to_room   = ( to  - 7)/2;
-            if from_room == to_room { return (0, false); }
-
-            let left_room = from_room.min(to_room);
-            let right_room = from_room.max(to_room);
-
-            // println!("left room: {}  right room: {}", left_room, right_room);
-            let dist = ((right_room+1) - (left_room+2) + from_bottom+2 + to_bottom+2) as u64;
-            // println!("   dist: {}  and checking {:?}", dist, &s[left_room+2..=right_room+1]);
-
-            return (dist, s[left_room+2..=right_room+1].iter().all(|&c| c == '.'));
-        }
-
-        if (from > 6 && to <=6) || (to > 6 && from <=6) {            
-            let hallway = from.min(to);
-            let room = (from.max(to) - 7)/2;
-            let bottom = (from.max(to) - 7) % 2;
-            if to > 6 {
-                let c = s[from];
-                if c == 'A' && room != 0 { return (0, false); }
-                if c == 'B' && room != 1 { return (0, false); }
-                if c == 'C' && room != 2 { return (0, false); }
-                if c == 'D' && room != 3 { return (0, false); }
-                if bottom == 0 && s[8+2*room] == '.' { return (0, false); }
-            }
-
-            if bottom > 0 && s[7+2*room] != '.' { return (0, false); }
-
-            if hallway > room+1 {
-                // println!("  checking: {:?}    from {}  to {}  hallway {}    room {}", &s[room+2..=hallway], from, to, hallway, room);
-                return ((bottom+2+hallway-(room+2)) as u64, s[room+2..=hallway].iter().all(|&c| c == '.'));
-            } else {
-                // println!("  checking: {:?}", &s[hallway..=room+1]);
-                return ((bottom+2+(room+1)-hallway) as u64, s[hallway..=room+1].iter().all(|&c| c == '.'));
-            } 
-        }
-        return (0, false);
-    };
 
     let start1 = Instant::now();
+    // solved by hand
+    let after1 = Instant::now();
+    if print_result {
+        println!("Part 1: {}", 15160);
+    }
 
-    let mut least_energy = u64::MAX;
+    let start2 = Instant::now();
+
+    let mut initial_state = State::new();
+    initial_state.bottom[0] = ['D', 'D', 'D', 'B'];
+    initial_state.bottom[1] = ['C', 'C', 'B', 'C'];
+    initial_state.bottom[2] = ['A', 'B', 'A', 'D'];
+    initial_state.bottom[3] = ['B', 'A', 'C', 'A'];
+
+    let mut least_energy = usize::MAX;
     let mut heap: BinaryHeap<State> = BinaryHeap::new();
-    let mut visited: HashMap<[char;15], u64> = HashMap::new();
-    heap.push(State{ energy: 0, positions: start_state });
+    let mut energies: HashMap<([char;11], [[char;4];4]), usize> = HashMap::new();
+    heap.push(initial_state);
     loop {
         if let Some(s) = heap.pop() {
-            if finished(&s.positions) {
+            if s.finished() {
                 least_energy = s.energy;
                 break;
             }
 
-            
-            (0..15).for_each(|from| {
-                let c = s.positions[from];
-                if c != '.' {
-                    (0..15).for_each(|to| {
-                        if from != to {
-                            let (dist, allow) = allowed(&s.positions, from, to);
-                            if allow {
-                                let mut next = s.clone();
-                                swap(&mut next.positions, from, to);
-                                next.energy += dist * cost(c);
-                                if !visited.contains_key(&next.positions) || next.energy < visited[&next.positions] {
-                                    println!("from: {:?} - {}", s.positions, s.energy);
-                                    println!("to:   {:?} - {}", next.positions, next.energy);
-                                    *visited.entry(next.positions).or_insert(0) = next.energy;
-                                    heap.push(next);
-                                }
-                            }
+            let movables = s.get_top_amphipods();
+            let top_targets = s.get_top_targets();
+
+            let mut insert_next_state = | s: &State | {
+                if energies.contains_key(&(s.top, s.bottom)) {
+                    if let Some(prev_energy) = energies.get(&(s.top, s.bottom)) {
+                        if s.energy < *prev_energy {
+                            *energies.entry((s.top, s.bottom)).or_insert(0) = s.energy;
+                            heap.push(s.clone());
                         }
-                    });
+                    } else {
+                        panic!();
+                    }
+                } else {
+                    energies.insert((s.top, s.bottom), s.energy);
+                    heap.push(s.clone());
+                }
+            };
+
+            top_targets.iter().for_each(|tt| {
+                ['A', 'B', 'C', 'D'].iter().for_each(|room| {
+                    let mut next_state = s.clone();
+                    if let Some(energy) = next_state.move_from_room(*room, *tt) {
+                        next_state.energy += energy;
+                        insert_next_state(&next_state);
+                    } 
+                });
+            });
+
+            ['A', 'B', 'C', 'D'].iter().for_each(|from_room| {
+                s.get_bottom_targets().iter().for_each(|to_room| {
+                    let mut next_state = s.clone();
+                    if let Some(energy) = next_state.move_room_to_room(*from_room, *to_room) {
+                        next_state.energy += energy;
+                        insert_next_state(&next_state);
+                    }
+                });
+            });
+
+            movables.iter().for_each(|(c, from)| {
+                let mut next_state = s.clone();
+                if let Some(energy) = next_state.move_to_room(*from, *c) {
+                    next_state.energy += energy;
+                    insert_next_state(&next_state);
                 }
             });
         } else {
             break;
         }
-    }
 
-    let after1 = Instant::now();
-    if print_result {
-        println!("Part 1: {}", least_energy);
     }
-
-    let start2 = Instant::now();
 
     let after2 = Instant::now();
     if print_result {
-        println!("Part 2: {}", 0);
+        println!("Part 2: {}", least_energy);
     }
 
     (
