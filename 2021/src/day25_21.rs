fd22469120d8a16c798d60aed4516d03688053fe
Arno Kamphuis <ik@arnokamphuis.nl>
2021-12-25 15:54:18 +0100

    Finished day 25, and therefore the whole year, of 2021

--- 2021/src/day25_21.rs
+++ 2021/src/day25_21.rs
@@ -6,19 +6,69 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     let start0 = Instant::now();
 
     let input_file: &str = if !real {
-        "./input/dayxx_21_test.txt"
+        "./input/day25_21_test.txt"
     } else {
-        "./input/dayxx_21_real.txt"
+        "./input/day25_21_real.txt"
     };
     let input = tools::get_input(String::from(input_file));
+
+    let mut field = input.iter().fold(vec![], |mut v, line| {
+        v.push(line.chars().collect::<Vec<char>>()); v
+    });
+
+    let dir_step = | prev: &Vec<Vec<char>>, dir: char | -> Vec<Vec<char>> {
+        let h = prev.len();
+        let w = prev[0].len();
+        let mut new = vec![vec![' ';w];h];
+        for i in 1..=h {
+            for j in 1..=w {
+                let curr_i = i % h;
+                let curr_j = j % w;
+                let prev_i = (i - (dir == 'v') as usize) % h;
+                let prev_j = (j - (dir == '>') as usize) % w;
+                if prev[prev_i][prev_j] == dir && prev[curr_i][curr_j] == '.' {
+                    new[curr_i][curr_j] = dir;
+                    new[prev_i][prev_j] = '.';
+                } else {
+                    if new[prev_i][prev_j] == ' ' {
+                        new[prev_i][prev_j] = prev[prev_i][prev_j];
+                    }
+                }
+            }
+        }
+        new
+    };
+
+    let step = | prev: &Vec<Vec<char>> | -> Vec<Vec<char>> {
+        dir_step(&dir_step(prev, '>'), 'v')
+    };
+    
+    // let print = | f: &Vec<Vec<char>> | {
+    //     f.iter().for_each(|v| {
+    //         v.iter().for_each(|c| print!("{}",c));
+    //         println!("");
+    //     });
+    //     println!("");
+    // };
 
     let after0 = Instant::now();
 
     let start1 = Instant::now();
 
+    let mut step_counter = 0;
+    loop {
+        step_counter += 1;
+        let new_field = step(&field);
+
+        if new_field == field {
+            break;
+        }
+        field = new_field;
+    }
+
     let after1 = Instant::now();
     if print_result {
-        println!("Part 1: {}", 0);
+        println!("Part 1: {}", step_counter);
     }
 
     let start2 = Instant::now();
