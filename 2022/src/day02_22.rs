5fca78bf536edcdb65bde862b368e5dce140c443
Arno Kamphuis <ik@arnokamphuis.nl>
2022-11-27 21:33:15 +0100

    Preparations done for AoC2022

---
+++ 2022/src/day02_22.rs
@@ -0,0 +1,36 @@
+use super::tools;
+use std::time::Instant;
+
+#[allow(dead_code)]
+pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
+    let start0 = Instant::now();
+
+    let input_file: &str = if !real {
+        "./input/day02_22_test.txt"
+    } else {
+        "./input/day02_22_real.txt"
+    };
+    let _input = tools::get_input(String::from(input_file));
+
+    let after0 = Instant::now();
+
+    let start1 = Instant::now();
+
+    let after1 = Instant::now();
+    if print_result {
+        println!("Part 1: {}", 0);
+    }
+
+    let start2 = Instant::now();
+
+    let after2 = Instant::now();
+    if print_result {
+        println!("Part 2: {}", 0);
+    }
+
+    (
+        after0.duration_since(start0).as_nanos(),
+        after1.duration_since(start1).as_nanos(),
+        after2.duration_since(start2).as_nanos(),
+    )
+}
