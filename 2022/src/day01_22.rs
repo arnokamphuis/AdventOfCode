40ead040a41a1fca3edc6a0c0b1da4d886620f71
Arno Kamphuis <arno.kamphuis@hu.nl>
2022-12-01 07:46:51 +0100

    Finished day 1 of 2022

--- 2022/src/day01_22.rs
+++ 2022/src/day01_22.rs
@@ -1,5 +1,6 @@
 use super::tools;
 use std::time::Instant;
+use itertools::sorted;
 
 #[allow(dead_code)]
 pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
@@ -10,22 +11,47 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     } else {
         "./input/day01_22_real.txt"
     };
-    let _input = tools::get_input(String::from(input_file));
+    let input = tools::get_input(String::from(input_file));
+
+    let mut elfs: Vec<Vec<usize>> = vec![];
+    let mut current_elf: Vec<usize> = vec![];
+    for line in &input {
+        if line.len() == 0 {
+            elfs.push(current_elf.clone());
+            current_elf = vec![];
+        } else {
+            current_elf.push(line.parse::<usize>().unwrap());
+        }
+    }
+    elfs.push(current_elf.clone());
 
     let after0 = Instant::now();
 
     let start1 = Instant::now();
 
+    let res1 = elfs.iter().map(|v| v.iter().sum::<usize>()).max().unwrap();
+
     let after1 = Instant::now();
     if print_result {
-        println!("Part 1: {}", 0);
+        println!("Part 1: {}", res1);
     }
 
     let start2 = Instant::now();
 
+    let res2 = sorted(
+        elfs
+            .iter()
+            .map(|v| v.iter().sum::<usize>())
+            .collect::<Vec<usize>>()
+        ).collect::<Vec<usize>>()
+        .iter()
+        .rev()
+        .take(3)
+        .sum::<usize>();
+
     let after2 = Instant::now();
     if print_result {
-        println!("Part 2: {}", 0);
+        println!("Part 2: {}", res2);
     }
 
     (
