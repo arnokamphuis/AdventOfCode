1b0632349b4e953b668ab1faeb2846b52a4a657b
Arno Kamphuis <ik@arnokamphuis.nl>
2022-12-02 07:45:45 +0100

    Finished day 2 of 2022, needs some refactoring

--- 2022/src/day02_22.rs
+++ 2022/src/day02_22.rs
@@ -10,22 +10,79 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     } else {
         "./input/day02_22_real.txt"
     };
-    let _input = tools::get_input(String::from(input_file));
+    let input = tools::get_input(String::from(input_file));
+
+    let rounds: Vec<(usize, usize)> = input
+        .iter()
+        .map(|line| {
+            let plays = line.split_whitespace().map(|i| i.chars().nth(0).unwrap() as usize).collect::<Vec<usize>>();
+            (plays[0]-64, plays[1]-87)
+        }).collect();
 
     let after0 = Instant::now();
 
     let start1 = Instant::now();
 
+    let score = | p1, p2 | -> usize {
+        match (p1,p2) {
+            (3,1) | (2,3) | (1,2) => 6,
+            (1,1) | (2,2) | (3,3) => 3,
+            _ => 0,
+        }
+    }; 
+
+    let res1 = rounds.iter().fold(0, |s, (p1,p2)| {
+        s + p2 + score(*p1,*p2)
+    });
+
     let after1 = Instant::now();
     if print_result {
-        println!("Part 1: {}", 0);
+        println!("Part 1: {}", res1);
     }
 
     let start2 = Instant::now();
 
+    let choose_win = | p1 | -> usize {
+        match p1 {
+            3 => 1,
+            2 => 3,
+            1 => 2,
+            _ => panic!("oops")
+        }
+    };
+
+    let choose_draw = | p1 | -> usize {
+        p1
+    };
+
+    let choose_lose = | p1 | -> usize {
+        match p1 {
+            1 => 3,
+            3 => 2,
+            2 => 1,
+            _ => panic!("oops")
+        }
+    };
+
+    let find_play = | p1, p2 | -> usize {
+        match p2 {
+            1 => choose_lose(p1),
+            2 => choose_draw(p1),
+            3 => choose_win(p1),
+            _ => panic!("oops")
+        }
+    };
+
+    let res2 = rounds
+        .iter()
+        .fold(0, |s, &(p1,p2)| {
+            let p = find_play(p1,p2);
+            s + p + score(p1,p)
+        });
+
     let after2 = Instant::now();
     if print_result {
-        println!("Part 2: {}", 0);
+        println!("Part 2: {}", res2);
     }
 
     (
