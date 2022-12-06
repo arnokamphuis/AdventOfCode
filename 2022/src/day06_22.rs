f3f92fa17cdc1f0117e97f747a2c42688766adeb
Arno Kamphuis <ik@arnokamphuis.nl>
2022-12-06 18:32:20 +0100

    Improved performance of day 6 of 2022 by removing the use of sets

--- 2022/src/day06_22.rs
+++ 2022/src/day06_22.rs
@@ -1,6 +1,5 @@
 use super::tools;
 use std::time::Instant;
-use std::collections::HashSet;
 
 #[allow(dead_code)]
 pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
@@ -13,26 +12,25 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     };
     let input = tools::get_input(String::from(input_file));
 
-    let processed_lines = input.iter().map(|line| {
-        line.chars().map(|c| c as usize).collect::<Vec<usize>>()
-    }).collect::<Vec<Vec<usize>>>();
+    let processed_line = input[0].chars().map(|c| c as usize - 'a' as usize).collect::<Vec<usize>>();
 
-    let different = | x: &[usize] | -> bool {
-        x.iter().map(|&v| v).collect::<HashSet<usize>>().len() == x.len()
+    let different = | x: &[usize], l: usize, set: &mut Vec<usize> | -> bool {
+        set[x[0]] -= 1;
+        set[x[l]] += 1;
+        set.iter().filter(|&&v| v >= 2).count() == 0
     };
     
     let find_different = | v: &Vec<usize>, l: usize | -> usize {
-        v.windows(l).position(|x| different(x) ).unwrap() + l
+        let mut set = vec![0usize;26];
+        v[0..l].iter().for_each(|&d| { set[d] += 1; });
+        v.windows(l+1).position(|x| different(x, l, &mut set) ).unwrap() + l + 1
     };
 
     let after0 = Instant::now();
 
     let start1 = Instant::now();
 
-    let res1 = processed_lines
-        .iter()
-        .map(|line| { find_different(line,4) })
-        .collect::<Vec<_>>()[0];
+    let res1 = find_different(&processed_line, 4);
 
     let after1 = Instant::now();
     if print_result {
@@ -41,10 +39,7 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
 
     let start2 = Instant::now();
 
-    let res2 = processed_lines
-        .iter()
-        .map(|line| { find_different(line,14) })
-        .collect::<Vec<_>>()[0];
+    let res2 = find_different(&processed_line, 14);
 
     let after2 = Instant::now();
     if print_result {
