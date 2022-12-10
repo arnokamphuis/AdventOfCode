9cb97e9e2fbc155894116665980437378fff0d4a
Arno Kamphuis <ik@arnokamphuis.nl>
2022-12-10 09:08:19 +0100

    Updated lines for movie making

--- 2022/make_movie.bat
+++ 2022/make_movie.bat
@@ -1,2 +1,6 @@
+ffmpeg -framerate 30 -i visible_%05d.png -s:v 720x720 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p day08-part1-30fps.mp4
+ffmpeg -framerate 30 -i place_%05d.png -s:v 720x720 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p day08-part2-30fps.mp4
+
+
 ffmpeg -framerate 1200 -i rope_%05d.png -s:v 720x720 -c:v libx264 -profile:v high -crf 20 -pix_fmt yuv420p day09-1200fps.mp4
 ffmpeg -i day09-30fps.mp4 -vf "fps=30,scale=720:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" -loop 0 day09-30fps.gif
