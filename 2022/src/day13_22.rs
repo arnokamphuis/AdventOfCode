2e746d4cc1e72641affbc645967c91be647cd513
Arno Kamphuis <ik@arnokamphuis.nl>
2022-12-13 08:04:07 +0100

    Started day 13 of 2022

--- 2022/src/day13_22.rs
+++ 2022/src/day13_22.rs
@@ -1,6 +1,21 @@
 use super::tools;
 use std::time::Instant;
 
+fn compare_vecs(v: &Vec<Vec<String>>) {
+    println!("{:?}", v);
+    
+}
+
+fn compare(lines: [&String;2]) -> bool {
+    let mut list_depth = [0usize, 0usize];
+    let mut index = [0usize, 0usize];
+    let items = lines.iter().map(|l| l[1..l.len()-1].split(",").map(|s| s.to_string()).collect::<Vec<String>>()).collect::<Vec<_>>();
+
+    compare_vecs(&items);
+    println!("");
+    true
+}
+
 #[allow(dead_code)]
 pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     let start0 = Instant::now();
@@ -10,15 +25,26 @@ pub fn run(real: bool, print_result: bool) -> (u128, u128, u128) {
     } else {
         "./input/day13_22_real.txt"
     };
-    let _input = tools::get_input(String::from(input_file));
+    let input = tools::get_input(String::from(input_file));
 
     let after0 = Instant::now();
 
     let start1 = Instant::now();
 
+    let mut pair_count = 0;
+    let nr_pairs = (input.len() as f32/3.0).ceil() as usize;
+    let mut correct = 0;
+
+    while pair_count < nr_pairs {
+        if compare([&input[&pair_count * 3 + 0], &input[pair_count * 3 + 1]]) {
+            correct += pair_count+1;
+        }
+        pair_count += 1;
+    }
+    
     let after1 = Instant::now();
     if print_result {
-        println!("Part 1: {}", 0);
+        println!("Part 1: {}", correct);
     }
 
     let start2 = Instant::now();
