bbddf53020c40f94230c66a43d439211b4368573
Arno Kamphuis <ik@arnokamphuis.nl>
2023-12-02 17:06:55 +0100

    Refactored day 2 of 2023 to use reduce() functionality

--- 2023/day02-23/day02-23.py
+++ 2023/day02-23/day02-23.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from functools import reduce
 import sys
 
@@ -15,36 +16,40 @@ for line_index in range(len(lines)):
     lines[line_index] = lines[line_index].split(": ")
     lines[line_index][0] = lines[line_index][0][5:]
     lines[line_index][1] = lines[line_index][1].split("; ")
-    lines[line_index][1] = [x.split(", ") for x in lines[line_index][1]]
+    lines[line_index][1] = [turns.split(", ") for turns in lines[line_index][1]]
+    lines[line_index][1] = [[turn.split() for turn in turns] for turns in lines[line_index][1]]
+
 
 def part1():
-    res = 0
     bag = { "red": 12, "green": 13, "blue": 14 }
-    for line in lines:
-        possible = reduce(
+    return reduce(
+        lambda x, y: x + y,
+        [
+            int(id) if reduce(
             lambda x, y: x and y,
             [ reduce(
                 lambda x, y: x and y,
-                [bag[c.split(" ")[1]] >= int(c.split(" ")[0]) for c in turn]
-                ) for turn in line[1]
-            ])
-        if possible:
-            res += int(line[0])
-    return res
+                [bag[color] >= int(count) for count, color in turn]
+                ) for turn in turns
+            ]) else 0
+            for id, turns in lines
+        ],
+        0
+    )
 
 def part2():
-    res = 0
-    for line in lines:
-        game_power = { "red": 0, "green": 0, "blue": 0 }
-        for turn in line[1]:
-            for i in range(len(turn)):
-                t = turn[i].split(" ")
-                game_power[t[1]] = max(game_power[t[1]], int(t[0]))
-        gp = 1
-        for v in game_power.values():
-            gp *= v
-        res += gp
-    return res
+    return reduce(
+        lambda x, y: x + y,
+        [ reduce(
+            lambda x, y: x * y,
+            reduce(
+                lambda x, y: { color: max(x.get(color,0), y.get(color,0)) for color in x },
+                [ dict([ (color, int(count)) for count, color in turn]) for turn in turns],
+                { "red": 0, "green": 0, "blue": 0 }
+                ).values(),
+            1) for id_, turns in lines
+        ], 0
+    )
 
 if runpart == 1 or runpart == 0:
     for run in range(runs):
