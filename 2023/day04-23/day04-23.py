3580e2270adb7ef87b7396194e84dc0273c2e09f
Arno Kamphuis <ik@arnokamphuis.nl>
2023-12-04 13:29:22 +0100

    Restructure such that reruns give same answer on day 4 of 2023

--- 2023/day04-23/day04-23.py
+++ 2023/day04-23/day04-23.py
@@ -17,23 +17,27 @@ count = defaultdict(int)
 winningcount = defaultdict(int)
 
 lines = [line.strip().split(": ") for line in text_file.readlines()]
-for id_, cards_ in lines:
-    id = int(id_[5:])
-    wcards, ycards = cards_.split(" | ")
-    winning = set(wcards.split())
-    yours = set(ycards.split())
-    wc = len(yours & winning)
 
-    count[id] = 1
-    winningcount[id] = wc
+def prep():
+    global cards, count, winningcount
+    for id_, cards_ in lines:
+        id = int(id_[5:])
+        wcards, ycards = cards_.split(" | ")
+        winning = set(wcards.split())
+        yours = set(ycards.split())
+        wc = len(yours & winning)
+        count[id] = 1
+        winningcount[id] = wc
 
 def part1():
+    global winningcount
     return reduce(
         lambda x, y: x + 2**(y-1), 
         filter( lambda x: x > 0, winningcount.values()), 
         0)
 
 def part2():
+    global count, winningcount
     for id, won in winningcount.items():
         for i in range(won):
             count[id+i+1] += count[id]
@@ -41,11 +45,13 @@ def part2():
 
 if runpart == 1 or runpart == 0:
     for run in range(runs):
+        prep()
         resp1 = part1()
     print("Part 1: {}".format(resp1))
 
 if runpart == 2 or runpart == 0:
     for run in range(runs):
+        prep()
         resp2 = part2()
     print("Part 2: {}".format(resp2))
     
