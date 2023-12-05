cae638db8067b08fc99fa5c40df6e18cf1e20310
Arno Kamphuis <ik@arnokamphuis.nl>
2023-12-05 21:14:15 +0100

    Finished part 2 of day 5 of 2023

--- 2023/day05-23/day05-23.py
+++ 2023/day05-23/day05-23.py
@@ -13,16 +13,32 @@ text_file = open("day05-{}.txt".format(runtype), "r")
 lines = [[ll.strip() for ll in line.split()] for line in text_file.readlines()]
 seeds = [int(seed) for seed in lines[0][1:]]
 allrules = []
+all_interval_rules = []
+
+def pairwise(iterable):
+    a = iter(iterable)
+    return zip(a, a)
+
+seeds_iterval = [ (s, s+d)  for s, d in pairwise(seeds) ]
 
 currentline = 1
 while currentline < len(lines):
     rules = []
+    interval_rules = []
     title = lines[currentline+1][0]
     currentline += 2
     while currentline < len(lines) and len(lines[currentline])>0:
-        rules.append([int(n) for n in lines[currentline]])
+        range_values = [int(n) for n in lines[currentline]]
+        rules.append(range_values)
+        interval_rules.append(
+            [
+                (int(range_values[1]), int(range_values[1]) + int(range_values[2])), 
+                (int(range_values[0]), int(range_values[0]) + int(range_values[2]))
+            ]
+        )
         currentline += 1
     allrules.append(rules)
+    all_interval_rules.append(interval_rules)
 
 def part1():
     global seeds
@@ -41,9 +57,52 @@ def part1():
         currentseeds = newseeds
     return min(currentseeds)
 
+def apply_ruleset(rules, currentseeds):
+    seeds = []
+    for (rule_src_start, rule_src_end), (rule_dst_start, rule_dst_end) in rules:
+        newseeds = []
+        while currentseeds:
+            seed_start, seed_end = currentseeds.pop()
+            delta = rule_dst_start - rule_src_start
+
+            leftside = (seed_start, min(seed_end, rule_src_start))
+            middle = (max(seed_start, rule_src_start), min(seed_end, rule_src_end))
+            rightside = (max(rule_src_end, seed_start), seed_end)
+            if leftside[1] > leftside[0]:
+                newseeds.append(leftside)
+            if middle[1] > middle[0]:
+                seeds.append((middle[0]+delta, middle[1]+delta))
+            if rightside[1] > rightside[0]:
+                newseeds.append(rightside)
+        currentseeds = newseeds
+    return seeds + currentseeds
 
 def part2():
-    return 0
+    global seeds_iterval
+    smallest = []
+    for seed in seeds_iterval:
+        currentseeds = [seed]
+        for rules in all_interval_rules:
+            seeds = []
+            for (rule_src_start, rule_src_end), (rule_dst_start, rule_dst_end) in rules:
+                newseeds = []
+                while currentseeds:
+                    seed_start, seed_end = currentseeds.pop()
+                    delta = rule_dst_start - rule_src_start
+
+                    leftside = (seed_start, min(seed_end, rule_src_start))
+                    middle = (max(seed_start, rule_src_start), min(seed_end, rule_src_end))
+                    rightside = (max(rule_src_end, seed_start), seed_end)
+                    if leftside[1] > leftside[0]:
+                        newseeds.append(leftside)
+                    if middle[1] > middle[0]:
+                        seeds.append((middle[0]+delta, middle[1]+delta))
+                    if rightside[1] > rightside[0]:
+                        newseeds.append(rightside)
+                currentseeds = newseeds
+            currentseeds = seeds + currentseeds
+        smallest.append(min(currentseeds)[0])
+    return min(smallest)
 
 if runpart == 1 or runpart == 0:
     for run in range(runs):
