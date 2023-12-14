110fe80eee1b03d32f26aad01c0376eabae67223
Arno Kamphuis <ik@arnokamphuis.nl>
2023-12-14 15:50:41 +0100

    Changed to a little smaller implementation of day 13 of 2023

--- 2023/day13-23/day13-23.py
+++ 2023/day13-23/day13-23.py
@@ -14,19 +14,19 @@ fields = [[[c for c in row] for row in map] for map in
     [block.split('\n') for block in 
     [block for block in text_file.read().split('\n\n')]]]
 
-def calculate_mirror_score(field, target):
+def calculate_mirror_oneway(field, target):
     res = 0
     R = len(field)
     C = len(field[0])
 
-    for c in range(1,C):
-        if sum([len(smudge) for smudge in [[((r,c-1-cc), (r,c+cc)) for r in range(R) if field[r][c-1-cc] != field[r][c+cc]] for cc in range(0,min(C-c,c))]]) == target:
-            res += c
-
     for r in range(1,R):
         if sum([len(smudge) for smudge in [[((r-1-rr,c), (r+rr,c)) for c in range(C) if field[r-1-rr][c] != field[r+rr][c]] for rr in range(0,min(R-r,r))]]) == target:
-            res += r * 100
+            res += r
     return res
+
+def calculate_mirror_score(field, target):
+    return (100 * calculate_mirror_oneway(field, target) + 
+        calculate_mirror_oneway(list(zip(*field)), target))
 
 def part1():
     return sum([calculate_mirror_score(field, 0) for field in fields])
